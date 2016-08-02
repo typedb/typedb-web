18d96cd97869e8d029e64c1e9cf6ea84fdd573f2
aelred <aelred717@gmail.com>
2016-08-02 13:54:51 +0100

    Fix appearance of variables and types

--- source/js/prism-graql.js
+++ source/js/prism-graql.js
@@ -9,18 +9,18 @@
             alias: 'string'
         },
         keyword: {
-            pattern: /(match|ask|insert|delete|select|isa|ako|plays-role|has-role|has-scope|datatype|is-abstract|has|value|id|of|limit|offset|order|by)(?![-\$a-zA-Z_0-9])/,
+            pattern: /((?:(?![-a-zA-Z_0-9]).)|^)(match|ask|insert|delete|select|isa|ako|plays-role|has-role|has-scope|datatype|is-abstract|has|value|id|of|limit|offset|order|by)(?![-a-zA-Z_0-9])/,
             alias: 'keyword'
         },
         special: {
             pattern: /graql>|results>|\.\.\./
         },
         variable: {
-            pattern: /\$[-a-zA-Z_0-9]+(?![-\$a-zA-Z_0-9])/,
+            pattern: /\$[-a-zA-Z_0-9]+/,
             alias: 'variable'
         },
         type: {
-            pattern: /[-a-zA-Z_][-a-zA-Z_0-9]*(?![-\$a-zA-Z_0-9])/,
+            pattern: /[-a-zA-Z_][-a-zA-Z_0-9]*/,
             alias: 'function'
         },
         number: {
