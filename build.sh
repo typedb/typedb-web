96b95c1265fa9208e45c0f60f6299814ad31c5d0
Haikal Pribadi <haikal@ontoit.io>
2016-08-18 00:20:22 +0100

    restructured documentation portal to move from docs folder to pages folder

--- build.sh
+++ build.sh
@@ -11,22 +11,21 @@ git submodule update --remote docs
 #grunt ??
 
 echo Building documentation pages
-export urlprefix=/docs
+export urlprefix=/pages
 cd docs
 rake build
 cd ..
 
-if [ -d public/docs ]; then
-    echo Removing old documentation files
-    rm -rf public/docs/
+if [ -d public/pages ]; then
+    echo Removing old pages
+    rm -rf public/pages/
 fi
 
 echo Copying files
-mkdir public/docs
+mkdir public/pages
 # OSX
 if [ $(dirname $HOME) == "/Users" ];then
-    cp -r docs/_site/* public/docs/
+    cp -r docs/_site/* public/pages/
 else
-    cp -rd docs/_site/* public/docs/
+    cp -rd docs/_site/* public/pages/
 fi
-
