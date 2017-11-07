a46cd22a7bfe7a84bd879e06601df5aca69bb3cc
Harjyot Singh <harjyot7@gmail.com>
2017-11-07 14:36:04 +0000

    new logo

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -6,7 +6,7 @@ const NavigationBar = () => (
   <div className="nav">
     <div className="container">
       <Link to="/" className="nav__logo">
-        <img src='assets/svg/grakn-logo.svg' alt="GRAKN AI" />
+        <img src='assets/svg/logo.svg' alt="GRAKN AI" />
       </Link>
       <NavigationMenu />
     </div>
