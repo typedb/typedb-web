3b0d4178715259e926c5bd6482bd09136e20eb47
Harjyot Singh <harjyot7@gmail.com>
2017-11-28 21:50:48 +0000

    Team info migration

---
+++ src/config/teamInfo.js
@@ -0,0 +1,130 @@
+module.exports = {
+  team: [
+    {
+      name: 'Haikal Pribadi',
+      position: 'Founder and CEO',
+      education: 'MPhil in Advanced Computer Science, University of Cambridge',
+      img: '/assets/img/team_photos/haikal2.png',
+      social: 'https://linkedin.com/in/haikalpribadi'
+    },
+    {
+      name: 'Precy Kwan, CPA',
+      position: 'Chief Operating Officer',
+      education: 'MPhil in Land Economy, University of Cambridge',
+      img: '/assets/img/team_photos/precy2.png',      
+      social: 'https://linkedin.com/in/precykwan'
+    },
+    {
+      name: 'Tomas Sabat',
+      position: 'Chief Revenue Officer',
+      education: 'MPhil in Politics, University of Cambridge',
+      img: '/assets/img/team_photos/tomas.png',      
+      social: 'https://linkedin.com/in/tomás-sabat-83265841'
+    },
+    {
+      name: 'Christian Jakenfelds',
+      position: 'Head of Sales',
+      education: 'BSc in Biochemistry, Imperial College London',
+      img: '/assets/img/team_photos/christian.png',      
+      social: 'https://linkedin.com/in/christianjakenfelds'
+    },
+    {
+      name: 'Dr. Domenico Corapi',
+      position: 'Lead Engineer',
+      education: 'PhD in Computer Science, Imperial College London',
+      img: '/assets/img/team_photos/domenico.png',      
+      social: 'https://linkedin.com/in/dcorapi'
+    },
+    {
+      name: 'Dr. Filipe Teixeira',
+      position: 'Lead Engineer',
+      education: 'PhD in Computational Neurodynamics, Imperial College London',
+      img: '/assets/img/team_photos/filipe2.png',      
+      social: 'https://linkedin.com/in/fppintoteixeira'
+    },
+    {
+      name: 'Felix Chapman',
+      position: 'Software Engineer',
+      education: 'MEng in Computer Science, University of Southampton',
+      img: '/assets/img/team_photos/felix2.png',      
+      social: 'https://linkedin.com/in/felixchapman'
+    },
+    {
+      name: 'Ganeshwara Putra',
+      position: 'Software Engineer',
+      education: 'MSc in Grid Computing, Universiteit van Amsterdam',
+      img: '/assets/img/team_photos/ganesh.png',      
+      social: 'https://linkedin.com/in/ganeshwara-herawan-hananda-6aa00213'
+    },
+    {
+      name: 'Harjyot Singh',
+      position: 'Software Engineer',
+      education: 'BSc in Artificial Intelligence, University of Edinburgh',
+      img: '/assets/img/team_photos/harjyot.png',      
+      social: 'https://www.linkedin.com/in/harjyot7'
+    },
+    {
+      name: 'Dr. Jason Liu',
+      position: 'Software Engineer',
+      education: 'PhD in Machine Learning, University of York',
+      img: '/assets/img/team_photos/jason2.png',      
+      social: 'https://linkedin.com/in/jasonxiaohuliu'
+    },
+    {
+      name: 'Dr. Kasper Piskorski',
+      position: 'Software Engineer',
+      education: 'PhD in Scientific Computing, University of Cambridge',
+      img: '/assets/img/team_photos/kasper3.png',      
+      social: 'https://linkedin.com/in/kasperpiskorski'
+    },
+    {
+      name: 'Michele Orsi',
+      position: 'Lead Engineer',
+      education: 'MSc in Software Engineering, University of Bologna',
+      img: '/assets/img/team_photos/michele.png',      
+      social: 'https://linkedin.com/in/michele-orsi'
+    },
+    {
+      name: 'Marco Scoppetta',
+      position: 'Software Engineer',
+      education: 'MSc in Engineering of Computing Systems, Politecnico di Milano',
+      img: '/assets/img/team_photos/marco2.png',      
+      social: 'https://linkedin.com/in/marcoscoppetta'
+    },
+    {
+      name: 'Dr. Michelangelo B.',
+      position: 'Knowledge Engineer',
+      education: 'PhD in Discrete Mathematics, University of Naples',
+      img: '/assets/img/team_photos/miko.png',      
+      social: 'https://linkedin.com/in/michelangelobucci'
+    },
+    {
+      name: 'Nick Dillon',
+      position: 'Business Operations',
+      education: 'MPhil in Urban Studies, University of Cambridge',
+      img: '/assets/img/team_photos/nick.png',      
+      social: 'https://linkedin.com/in/nicholas-d-700411145'
+    },
+    {
+      name: 'Nicolas Powell',
+      position: 'ML Intern Engineer',
+      education: 'BS in Mathematical and Computational Science, Stanford University',
+      img: '/assets/img/team_photos/nicolas.png',      
+      social: 'https://linkedin.com/in/nicolas-powell-6b744989'
+    },
+    {
+      name: 'Oscar Darwin',
+      position: 'ML Intern Engineer',
+      education: 'BA in Maths and Computer Science, University of Oxford',
+      img: '/assets/img/team_photos/oscar.png',      
+      social: 'https://linkedin.com/in/oscar-darwin-44189076'
+    },
+    {
+      name: 'Thanh Pham',
+      position: 'Cluster Engineer',
+      education: 'MSc in Financial Mathematics, Cass Business School',
+      img: '/assets/img/team_photos/thanh.png',      
+      social: 'https://linkedin.com/in/thanh-pham-bb506115'
+    },
+  ]
+}
