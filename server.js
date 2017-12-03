8ae1d092ed1db4076141a34537c03e17427ea008
Harjyot Singh <harjyot7@gmail.com>
2017-12-03 13:32:51 +0000

    Added APIinviter functions

--- server.js
+++ server.js
@@ -1,13 +1,147 @@
 const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
+const unirest = require('unirest');
 
 const app = express();
 
-const port = process.env.PORT ? process.env.PORT : 3000;
+const port = process.env.PORT ? process.env.PORT : 3001;
 const dist = path.join(__dirname, 'dist');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 app.use('/', express.static(dist));
+
+function handleSlackInvite(userEmail) {
+  unirest.post('https://grakn-slackin.herokuapp.com/invite')
+      .headers({
+          'Content-Type': 'application/json'
+      })
+      .send({
+          "coc": 0,
+          "channel": "community",
+          "email": userEmail
+      })
+      .end(function(response) {
+          console.log("Just sent a new Slack invite, response:");
+          console.log(response.body);
+      });
+}
+
+function handleTPInvite(userEmail, userName, userSurname) {
+  unirest.post('https://work.grakn.ai/api/v1/UserStories?access_token=NTQ6Rm1XMEY4Z2VoelU4WE1PVjJGSVdiU3VSYktaNGJneG1naVVLZ2pyYjJzST0=&format=json')
+      .headers({
+          'Content-Type': 'application/json'
+      })
+      .send({
+          "Name": userName + " " + userSurname,
+          "Project": {
+              "Id": 16395
+          },
+          "Tags": "Community",
+    "Team": 276,
+    "ResponsibleTeam": 276,
+          "CustomFields": [{
+              "Name": "Email",
+              "Value": userEmail
+          }]
+
+      })
+      .end(function(response) {
+          console.log("Just added user to TargetProcess, response:");
+          console.log(JSON.stringify(response));
+      });
+}
+
+function handleMailChimpInvite(userEmail, userName, userSurname) {
+  unirest.post('https://us8.api.mailchimp.com/3.0/lists/3742a20dc0/members/')
+      .headers({
+          'Content-Type': 'application/json'
+      })
+      .auth({
+          user: 'GraknLabs',
+          pass: '023643a9dd794a291a676defa42ef588-us8',
+          sendImmediately: true
+      })
+      .send({
+          "coc": 0,
+          "merge_fields": {
+              "FNAME": userName || "",
+              "LNAME": userSurname || ""
+          },
+          "email_address": userEmail,
+          "status": "subscribed"
+      })
+      .end(function(response) {
+          console.log("Just sent a new MailChimp invite, response:");
+          console.log(response.body);
+      });
+}
+
+function handleGraknForumInvite(userEmail) {
+  unirest.post('https://discuss.grakn.ai/invites?api_key=298220c1f63076c58c642d4701ed384833dbaac8fb0ccf9411f52505ed527605&api_username=haikal')
+    .headers({
+        'Content-Type': 'application/json'
+    })
+    .send({
+        "email": userEmail
+    })
+    .end(function(response) {
+        console.log("Just sent a new Forum invite, response:");
+        console.log(response.body);
+    });
+}
+
+app.post('/invite/slack', function(req, res) {
+  if (req.body.email == undefined) {
+      res.status(400).send('No "email" field provided');
+      return;
+  }
+  handleSlackInvite(req.body.email);
+
+  res.header("Access-Control-Allow-Origin", "*");
+  res.status(200).send("Invite successfully sent");
+});
+
+app.post('/invite/tp', function(req, res) {
+  if (req.body.email == undefined) {
+      res.status(400).send('No "email" field provided');
+      return;
+  }
+  handleTPInvite(req.body.email, req.body.name, req.body.surname);
+  // handleMailChimpInvite(req.body.email, req.body.name, req.body.surname);
+  // handleGraknForumInvite(req.body.email);
+
+  res.header("Access-Control-Allow-Origin", "*");
+  res.status(200).send("Invite successfully sent");
+});
+
+app.post('/invite/mailchimp', function(req, res) {
+  if (req.body.email == undefined) {
+      res.status(400).send('No "email" field provided');
+      return;
+  }
+  handleMailChimpInvite(req.body.email, req.body.name, req.body.surname);
+  // handleGraknForumInvite(req.body.email);
+
+  res.header("Access-Control-Allow-Origin", "*");
+  res.status(200).send("Invite successfully sent");
+});
+
+app.post('/invite/forum', function(req, res) {
+  if (req.body.email == undefined) {
+      res.status(400).send('No "email" field provided');
+      return;
+  }
+  handleGraknForumInvite(req.body.email);
+
+  res.header("Access-Control-Allow-Origin", "*");
+  res.status(200).send("Invite successfully sent");
+});
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(dist, 'index.html'));
