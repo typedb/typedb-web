38b08a61fe823724a73e4db1f5f61c7ffb5eced1
Harjyot Singh <harjyot7@gmail.com>
2017-12-05 02:37:13 +0000

    Community Page links CSS global styled

--- src/pages/CommunityPage.js
+++ src/pages/CommunityPage.js
@@ -44,35 +44,35 @@ class CommunityPage extends Component {
                 <div className="community__content__links__col__item">
                   <span className="community__content__links__col__item__header">Talk to us on Slack</span>
                   <span className="community__content__links__col__item__text">
-                  We’d love to help you get up to speed with Grakn. Get in touch with the team and community on our <Link to="/slack">Slack Channel</Link>
+                  We’d love to help you get up to speed with Grakn. Get in touch with the team and community on our <Link to="/slack" className="animated__link animated__link--purple">Slack Channel</Link>
                   </span>                  
                 </div>
                 <div className="community__content__links__col__item">
                   <span className="community__content__links__col__item__header">Get involved on GitHub
                   </span>
                   <span className="community__content__links__col__item__text">
-                  Report a bug, request new features, submit a patch and follow our development on our <a href={graknRoutes.github} target="_blank">GitHub Repository</a>
+                  Report a bug, request new features, submit a patch and follow our development on our <a href={graknRoutes.github} target="_blank" className="animated__link animated__link--purple">GitHub Repository</a>
                   </span>                  
                 </div>
                 <div className="community__content__links__col__item">
                   <span className="community__content__links__col__item__header">Start a discussion                  
                   </span>
                   <span className="community__content__links__col__item__text">
-                  When you’re stuck on a problem, often the most effective remedy is to collaborate. Ask your question on <a href={graknRoutes.stack} target="_blank">Stackoverflow</a> or discuss your problem in our <a href={graknRoutes.discuss} target="_blank">Discussion Forum</a>.
+                  When you’re stuck on a problem, often the most effective remedy is to collaborate. Ask your question on <a href={graknRoutes.stack} target="_blank" className="animated__link animated__link--purple">Stackoverflow</a> or discuss your problem in our <a href={graknRoutes.discuss} className="animated__link animated__link--purple">Discussion Forum</a>.
                   </span>                  
                 </div>
                 <div className="community__content__links__col__item">
                   <span className="community__content__links__col__item__header">Join our Facebook Group                  
                   </span>
                   <span className="community__content__links__col__item__text">
-                  We love to stay up to date with things happening within our community. Join our <a href={graknRoutes.facebook} target="_blank">Facebook Group</a> and meet the rest of our community members.
+                  We love to stay up to date with things happening within our community. Join our <a href={graknRoutes.facebook} target="_blank" className="animated__link animated__link--purple">Facebook Group</a> and meet the rest of our community members.
                   </span>                  
                 </div>
                 <div className="community__content__links__col__item">
                   <span className="community__content__links__col__item__header">Build with Grakn                  
                   </span>
                   <span className="community__content__links__col__item__text">
-                  We have a growing list of project ideas. If you need inspiration and want to build something cool, take a look at our <a href="https://docs.grakn.ai/documentation/examples/projects.html" target="_blank">suggestions</a> or send us your own ideas via the <a href={graknRoutes.github} target="_blank">Discussion Forum.</a>
+                  We have a growing list of project ideas. If you need inspiration and want to build something cool, take a look at our <a href="https://docs.grakn.ai/documentation/examples/projects.html" target="_blank" className="animated__link animated__link--purple">suggestions</a> or send us your own ideas via the <a href={graknRoutes.github} target="_blank" className="animated__link animated__link--purple">Discussion Forum.</a>
                   </span>                  
                 </div>
               </div>
@@ -80,7 +80,7 @@ class CommunityPage extends Component {
                 <div className="community__content__links__col__item">
                   <span className="community__content__links__col__item__header">Join our Meetups!</span>
                   <span className="community__content__links__col__item__text">
-                  Learn how graph technologies could help you work more intelligently with highly interconnected data. We will talk about best practices in complex data modelling, graph visualisation, graph analytics, graph deployments in the cloud, as well as their applications for intelligent systems and AI applications. Join our <a href={graknRoutes.meetups} target="_blank">meetups</a>
+                  Learn how graph technologies could help you work more intelligently with highly interconnected data. We will talk about best practices in complex data modelling, graph visualisation, graph analytics, graph deployments in the cloud, as well as their applications for intelligent systems and AI applications. Join our <a href={graknRoutes.meetups} target="_blank" className="animated__link animated__link--purple">meetups</a>
                   </span>                  
                 </div>
                 <div className="community__content__links__col__item">
