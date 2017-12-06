5b67a8f34cc059500b5dc0cbdd1865821989a8dc
Harjyot Singh <harjyot7@gmail.com>
2017-12-06 14:27:59 +0000

    Bare bones version of graph visualisation

--- src/components/Visualiser.js
+++ src/components/Visualiser.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import classNames from 'classnames';
 import Split from 'split.js';
 import { keys } from 'lodash';
+
+import vis from 'vis';
 
 const Prism = require('prismjs');
 const graqlHighlighter = require('helpers/prism-graql.js').graql;
@@ -13,11 +16,15 @@ class Visualiser extends Component {
     super(props);
     this.state = {
       selected: keys(visualiserItems)[0],
+      network: null,
     };
     this.change = this.change.bind(this);
+    this.drawGraph = this.drawGraph.bind(this);
+    this.initializeGraph = this.initializeGraph.bind(this);
   }
 
   componentDidMount() {
+    this.initializeGraph();    
     Split(['#visualiser-code', '#visualiser-graph'], {
       sizes: [50, 50],
       gutterSize: 14,
@@ -36,7 +43,71 @@ class Visualiser extends Component {
   change(i) {
     this.setState({
       selected: i,
+    }, this.drawGraph(visualiserItems[i].graph));
+  }
+
+  initializeGraph() {
+    const container = this.graphContainer;    
+    const g = {
+      nodes: new vis.DataSet(),
+      edges: new vis.DataSet(),
+    }
+    const options = {
+      nodes: {
+        borderWidth: 1,
+        size: 10,
+        color: {
+          background: '#667fc9'
+        },
+        font: {
+          color: '#000',
+
+        },
+        shape: 'box',
+        fixed: true
+      },
+      edges: {
+        color: 'white',
+        arrows: 'to',
+      },
+    };
+    const network = new vis.Network(container, g, options);
+    network.on('resize', function() {
+      network.fit();
     });
+    this.setState({
+      network: network,
+    },this.drawGraph(visualiserItems[this.state.selected].graph, network));
+  }
+
+  drawGraph(dataset,alternateNetwork) {
+    const network = alternateNetwork? alternateNetwork : this.state.network;
+    const nodes = [];
+    const edges = [];
+    const container = this.graphContainer;
+    dataset.nodes.map((item, index) => {
+      nodes.push({
+        id: index,
+        group: item.type,
+        label: item.text,
+        x: (item.cx / 100 ) * container.offsetWidth,
+        y: (item.cy / 100 ) * 514
+      });
+    });
+    dataset.edges.map((item, index) => {
+      edges.push({
+        from: item.source,
+        to: item.target,
+        label: item.text
+      });
+    });
+    const g = {
+      nodes: new vis.DataSet(nodes),
+      edges: new vis.DataSet(edges),
+    };
+    network.setData(g);
+    network.redraw();
+    network.fit();
   }
 
   render() {
@@ -62,8 +133,7 @@ class Visualiser extends Component {
                 <code dangerouslySetInnerHTML={{__html: code}}/>
               </pre>
           </div>
-          <div id="visualiser-graph" className="visualiser__content__graph">
-            Graph
+          <div id="visualiser-graph" className="visualiser__content__graph" ref={(container) => this.graphContainer = container}>
           </div>
         </div>
       </div>
--- src/config/visualiserItems.js
+++ src/config/visualiserItems.js
@@ -19,7 +19,46 @@ employment sub relationship,
 name sub attribute,
 	datatype string;
 
-commit `},
+commit `,
+graph: {
+	nodes: [
+			{
+					type: 'relationship-type',
+					text: 'employment',
+					r: 10,
+					cx: 50,
+					cy: 20
+			},
+			{
+					type: 'entity-type',
+					text: 'person',
+					r: 10,
+					cx: 20,
+					cy: 50
+			},
+			{
+					type: 'entity-type',
+					text: 'company',
+					r: 10,
+					cx: 80,
+					cy: 50
+			},
+			{
+					type: 'attribute-type',
+					text: 'name',
+					r: 10,
+					cx: 50,
+					cy: 80
+			}
+	],
+	edges: [
+			{source: 0, target: 1, text: 'employee'},
+			{source: 0, target: 2, text: 'employer'},
+			{source: 1, target: 3, text: 'has'},
+			{source: 2, target: 3, text: 'has'}
+	]
+}
+},
 Types: { code: `
 # Type Hierarchies
 
@@ -37,7 +76,69 @@ teacher sub person;
 supervisor sub teacher;
 professor sub teacher;
 
-commit ` },
+commit `,
+graph: {
+	nodes: [
+			{
+					type: 'entity-type',
+					text: 'person',
+					r: 10,
+					cx: 50,
+					cy: 20
+			},
+			{
+					type: 'entity-type',
+					text: 'student',
+					r: 10,
+					cx: 30,
+					cy: 40
+			},
+			{
+					type: 'entity-type',
+					text: 'teacher',
+					r: 10,
+					cx: 70,
+					cy: 40
+			},
+			{
+					type: 'entity-type',
+					text: 'undergrad',
+					r: 10,
+					cx: 20,
+					cy: 80
+			},
+			{
+					type: 'entity-type',
+					text: 'postgrad',
+					r: 10,
+					cx: 40,
+					cy: 80
+			},
+			{
+					type: 'entity-type',
+					text: 'supervisor',
+					r: 10,
+					cx: 60,
+					cy: 80
+			},
+			{
+					type: 'entity-type',
+					text: 'professor',
+					r: 10,
+					cx: 80,
+					cy: 80
+			}
+	],
+	edges: [
+			{source: 1, target: 0, text: 'sub'},
+			{source: 2, target: 0, text: 'sub'},
+			{source: 3, target: 1, text: 'sub'},
+			{source: 4, target: 1, text: 'sub'},
+			{source: 5, target: 2, text: 'sub'},
+			{source: 6, target: 2, text: 'sub'}
+	]
+}
+ },
 Ternary: { code: `
 # Ternary Relationships
 
@@ -49,7 +150,53 @@ $z isa figure, has name "Jack";
 ($x, $y, $z) isa cast,
 	has billing-number 1;
 
-commit `},
+commit `,
+graph: {
+	nodes: [
+			{
+					type: 'entity',
+					text: 'person: Leonardo',
+					r: 10,
+					cx: 50,
+					cy: 20
+			},
+			{
+					type: 'relationship',
+					text: 'cast',
+					r: 8,
+					cx: 50,
+					cy: 50
+			},
+			{
+					type: 'entity',
+					text: 'movie: Titanic',
+					r: 10,
+					cx: 20,
+					cy: 70
+			},
+			{
+					type: 'entity',
+					text: 'figure: Jack',
+					r: 10,
+					cx: 80,
+					cy: 70
+			},
+			{
+					type: 'attribute',
+					text: 'billing: 1',
+					r: 8,
+					cx: 50,
+					cy: 80
+			}
+	],
+	edges: [
+			{source: 1, target: 0, text: 'actor'},
+			{source: 1, target: 2, text: 'casted'},
+			{source: 1, target: 3, text: 'character'},
+			{source: 1, target: 4, text: 'has'}
+	]
+}
+},
 Nested: { code:`
 # Nested Relationships
 
@@ -62,7 +209,53 @@ $m (wife: $a, husband: $b);
 $c isa city, has name "London";
 (located: $m, locating: $c) isa location;
 
-commit`},
+commit`,
+graph: {
+	nodes: [
+			{
+					type: 'entity',
+					text: 'person: Alice',
+					r: 10,
+					cx: 20,
+					cy: 20
+			},
+			{
+					type: 'entity',
+					text: 'person: Bob',
+					r: 10,
+					cx: 80,
+					cy: 20
+			},
+			{
+					type: 'relationship',
+					text: 'marriage',
+					r: 8,
+					cx: 50,
+					cy: 35
+			},
+			{
+					type: 'relationship',
+					text: 'location',
+					r: 8,
+					cx: 35,
+					cy: 65
+			},
+			{
+					type: 'entity',
+					text: 'city: London',
+					r: 10,
+					cx: 65,
+					cy: 80
+			}
+	],
+	edges: [
+			{source: 2, target: 0, text: 'wife'},
+			{source: 2, target: 1, text: 'husband'},
+			{source: 3, target: 2, text: 'located'},
+			{source: 3, target: 4, text: 'locating'}
+	]
+}
+},
 Rules: { code:`
 # Schema Rules
 
@@ -77,7 +270,62 @@ then {
 	(located: $x, locating: $z);
 }
 
-commit `},
+commit `,
+graph: {
+	nodes: [
+			{
+					type: 'relationship',
+					text: 'location',
+					r: 8,
+					cx: 50,
+					cy: 20
+			},
+			{
+					type: 'entity',
+					text: 'ward: Kings Cross',
+					r: 10,
+					cx: 20,
+					cy: 50
+			},
+			{
+					type: 'entity',
+					text: 'city: London',
+					r: 10,
+					cx: 50,
+					cy: 50
+			},
+			{
+					type: 'entity',
+					text: 'country: UK',
+					r: 10,
+					cx: 80,
+					cy: 50
+			},
+			{
+					type: 'relationship',
+					text: 'location',
+					r: 8,
+					cx: 35,
+					cy: 80
+			},
+			{
+					type: 'relationship',
+					text: 'location',
+					r: 8,
+					cx: 65,
+					cy: 80
+			}
+	],
+	edges: [
+			{source: 0, target: 1, text: 'located'},
+			{source: 0, target: 3, text: 'locating'},
+			{source: 4, target: 1, text: 'located'},
+			{source: 4, target: 2, text: 'locating'},
+			{source: 5, target: 2, text: 'located'},
+			{source: 5, target: 3, text: 'locating'}
+	]
+}
+},
 Inference: { code: `
 # Reasoning OLTP
 
@@ -92,7 +340,101 @@ get $a;
 results>>
 
 $a isa teacher, has name "Alice";
-$a isa postgrad, has name "Bob";` },
+$a isa postgrad, has name "Bob";`,
+graph: {
+	nodes: [
+			{
+					type: 'entity',
+					text: 'teacher: Alice',
+					r: 10,
+					cx: 20,
+					cy: 20
+			},
+			{
+					type: 'entity',
+					text: 'city: Cambridge',
+					r: 10,
+					cx: 50,
+					cy: 20
+			},
+			{
+					type: 'entity',
+					text: 'country: UK',
+					r: 10,
+					cx: 80,
+					cy: 20
+			},
+			{
+					type: 'relationship',
+					text: 'location',
+					r: 8,
+					cx: 35,
+					cy: 35
+			},
+			{
+					type: 'relationship',
+					text: 'location',
+					r: 8,
+					cx: 65,
+					cy: 35
+			},
+			{
+					type: 'relationship',
+					text: 'location',
+					r: 8,
+					cx: 75,
+					cy: 50
+			},
+			{
+					type: 'relationship',
+					text: 'location',
+					r: 8,
+					cx: 35,
+					cy: 65
+			},
+			{
+					type: 'relationship',
+					text: 'location',
+					r: 8,
+					cx: 65,
+					cy: 65
+			},
+			{
+					type: 'entity',
+					text: 'postgrad: Bob',
+					r: 10,
+					cx: 20,
+					cy: 80
+			},
+			{
+					type: 'entity',
+					text: 'ward: Kings Cross',
+					r: 10,
+					cx: 50,
+					cy: 80
+			},
+			{
+					type: 'entity',
+					text: 'city: London',
+					r: 10,
+					cx: 80,
+					cy: 80
+			}
+	],
+	edges: [
+			{source: 3, target: 0, text: 'located'},
+			{source: 3, target: 1, text: 'locating'},
+			{source: 4, target: 1, text: 'located'},
+			{source: 4, target: 2, text: 'locating'},
+			{source: 6, target: 8, text: 'located'},
+			{source: 6, target: 9, text: 'locating'},
+			{source: 7, target: 9, text: 'located'},
+			{source: 7, target: 10, text: 'locating'},
+			{source: 5, target: 2, text: 'locating'},
+			{source: 5, target: 10, text: 'located'}
+	]
+}
+ },
 Analytics: { code:`
 # Analytics OLAP
 
@@ -101,5 +443,90 @@ compute cluster in movie, cast, person; member;
 results>>
 
 "Titanic": {"Titanic", "Kate", "Leonardo"}
-"Al Pacino": {"Al Pacino", "Godfather", "Scarface"}`}
+"Al Pacino": {"Al Pacino", "Godfather", "Scarface"}`,
+graph: {
+	nodes: [
+			{
+					type: 'entity',
+					text: 'movie: Titanic',
+					r: 10,
+					cx: 20,
+					cy: 20
+			},
+			{
+					type: 'entity',
+					text: 'person: Leonardo',
+					r: 10,
+					cx: 60,
+					cy: 20
+			},
+			{
+					type: 'entity',
+					text: 'person: Al Pacino',
+					r: 10,
+					cx: 80,
+					cy: 20
+			},
+			{
+					type: 'relationship',
+					text: 'cast',
+					r: 8,
+					cx: 40,
+					cy: 25
+			},
+			{
+					type: 'relationship',
+					text: 'cast',
+					r: 8,
+					cx: 25,
+					cy: 50
+			},
+			{
+					type: 'relationship',
+					text: 'cast',
+					r: 8,
+					cx: 55,
+					cy: 50
+			},
+			{
+					type: 'relationship',
+					text: 'cast',
+					r: 8,
+					cx: 75,
+					cy: 50
+			},
+			{
+					type: 'entity',
+					text: 'person: Kate',
+					r: 10,
+					cx: 20,
+					cy: 80
+			},
+			{
+					type: 'entity',
+					text: 'movie: Scarface',
+					r: 10,
+					cx: 50,
+					cy: 80
+			},
+			{
+					type: 'entity',
+					text: 'movie: The Godfather',
+					r: 10,
+					cx: 80,
+					cy: 80
+			}
+	],
+	edges: [
+			{source: 3, target: 0, text: 'casted'},
+			{source: 3, target: 1, text: 'actor'},
+			{source: 4, target: 0, text: 'casted'},
+			{source: 4, target: 7, text: 'actor'},
+			{source: 5, target: 2, text: 'actor'},
+			{source: 5, target: 8, text: 'casted'},
+			{source: 6, target: 2, text: 'actor'},
+			{source: 6, target: 9, text: 'casted'},
+	]
+}
+}
 }
