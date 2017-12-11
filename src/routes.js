a2a953f82f356abbd6c22dffa38d61c73791b1ce
Harjyot Singh <harjyot7@gmail.com>
2017-12-11 18:13:38 +0000

    Added google analytics

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import ReactGA from 'react-ga';
 import HomePage from 'pages/HomePage';
 import AboutPage from 'pages/AboutPage';
 import SlackPage from 'pages/SlackPage';
@@ -7,17 +9,36 @@ import CareersPage from 'pages/CareersPage';
 import CommunityPage from 'pages/CommunityPage';
 import NoPage from 'pages/NoPage';
 
-const Main = () => (
-  <main className="main">
-    <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/about" component={AboutPage} />
-      <Route exact path="/slack" component={SlackPage} />
-      <Route exact path="/careers" component={CareersPage} />
-      <Route exact path="/community" component={CommunityPage} />
-      <Route component={NoPage} />
-    </Switch>
-  </main>
-);
+class Main extends Component {
+  componentDidMount() {
+    ReactGA.initialize('UA-72414051-1'); 
+  }
 
-export default Main;
+  componentWillReceiveProps(newProps) {
+    if (this.props.path !== newProps.path) {
+      console.log("Path changed");
+      ReactGA.pageview(newProps.path);
+    }
+  }
+  render() {
+    return (
+      <main className="main">
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/about" component={AboutPage} />
+          <Route exact path="/slack" component={SlackPage} />
+          <Route exact path="/careers" component={CareersPage} />
+          <Route exact path="/community" component={CommunityPage} />
+          <Route component={NoPage} />
+        </Switch>
+      </main>
+    )
+  }
+};
+
+const mapStateToProps = (state) => (
+  {
+    path: state.router.location.pathname,
+  }
+);
+export default connect(mapStateToProps)(Main);
