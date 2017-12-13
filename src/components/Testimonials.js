d13dc6d45dd86022e7b2dc00f5c71448e976933e
Harjyot Singh <harjyot7@gmail.com>
2017-12-13 22:27:07 +0000

    Updated testimonial

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -4,7 +4,7 @@ import PagingComponent from './PagingComponent';
 const testimonials = [
   {name: 'Michael Bishop', company: 'CTO, Alpha Vertex', img: '/assets/img/bishop.jpg', review: '“Grakn significantly streamlines our knowledge engineering process. Grakn’s expressive schema allows us to verify the logical consistency of patterns detected by our learning algorithms and improve accuracy”'},
   {name: 'Radouane Oudrhiri', company: 'CTO, Eagle Genomics', img: '/assets/img/oudrhiri.jpg', review: '“Grakn\'s query language, Graql, should be the de facto language for any graph representation because of two things: the semantic expressiveness of the language and the optimisation of query execution.”'},
-  {name: 'Gunnar Kleemann', company: 'Co-Founder, Berkeley Data Science Group', img: '/assets/img/gunnar.jpg', review: '“When working with network structures, such as Biology, interactions between objects are complex and nuanced. Grakn interprets these structures natively, and allow us to discover novel answers very quickly.”'},
+  {name: 'Gunnar Kleemann', company: 'Co-Founder, Berkeley Data Science Group', img: '/assets/img/gunnar.jpg', review: '“When working with network structures, such as gene networks, interactions between objects are complex and nuanced. Grakn interprets these structures natively, and allow us to discover novel answers very quickly”'},
 ];
 
 const Testimonials = () => (
