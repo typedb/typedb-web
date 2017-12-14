e5ff2ccbc898f10ed551ff777727ecd4b328e182
Harjyot Singh <harjyot7@gmail.com>
2017-12-14 23:59:18 +0000

    Changes include: 1. Haikal's beauty pic 2. Support Form removed labels changed styles 3. Support & Services Page padding / css fixes

--- src/components/SupportForm.js
+++ src/components/SupportForm.js
@@ -58,45 +58,27 @@ class SupportForm extends Component {
         <Form ref={c => { this.form = c }}>
           <div className="support-form__row">
             <div className="support-form__row__item">
-              <label className="support-form__label">
-                First Name
-              </label>
-              <Input className="support-form__input" placeholder='Enter your First Name' name='firstname' validations={[required]}/>
+              <Input className="support-form__input" placeholder='First Name' name='firstname' validations={[required]}/>
             </div>
             <div className="support-form__row__item">
-              <label className="support-form__label">
-                Last Name
-              </label>
-              <Input className="support-form__input" placeholder='Enter your Last Name' name='lastname' validations={[required]}/>
+              <Input className="support-form__input" placeholder='Last Name' name='lastname' validations={[required]}/>
             </div>
           </div>
           <div className="support-form__row">
             <div className="support-form__row__item">
-              <label className="support-form__label">
-                Email
-              </label>
-              <Input className="support-form__input" placeholder='Enter your email' name='email' validations={[required, email]}/>
+              <Input className="support-form__input" placeholder='Email' name='email' validations={[required, email]}/>
             </div>
             <div className="support-form__row__item">
-              <label className="support-form__label">
-                Phone Number
-              </label>
-              <Input className="support-form__input" placeholder='Enter your Phone Number' name='phone' validations={[required]}/>
+              <Input className="support-form__input" placeholder='Phone Number' name='phone' validations={[required]}/>
             </div>
           </div>
           <div className="support-form__row">
             <div className="support-form__row__item">
-              <label className="support-form__label">
-                Company
-              </label>
-              <Input className="support-form__input" placeholder='Enter your company' name='company' validations={[required]}/>
+              <Input className="support-form__input" placeholder='Company' name='company' validations={[required]}/>
             </div>
             <div className="support-form__row__item">
-              <label className="support-form__label">
-                Job Function
-              </label>
               <Select className="support-form__input support-form__input__select" value='' name='job' validations={[required]}>
-                <option value=''>Choose your job</option>
+                <option value=''>Job function</option>
                 <option value='software enginner'>Software Engineer</option>
                 <option value='director'>Director / Development Manager</option>
                 <option value='it operations'>IT/Dev Operations</option>
@@ -114,11 +96,8 @@ class SupportForm extends Component {
           </div>
           <div className="support-form__row">
             <div className="support-form__row__item">
-              <label className="support-form__label">
-                Stage of development
-              </label>
               <Select className="support-form__input support-form__input__select" value='' name='stage' validations={[required]}>
-                <option value=''>Choose your stage</option>
+                <option value=''>Stage of development</option>
                 <option value='discovery'>Discovery phase</option>
                 <option value='installed'>Just installed</option>
                 <option value='development'>Development</option>
@@ -154,11 +133,9 @@ class SupportForm extends Component {
                 </div>
               </div>
               <div className="support-form__row__item">
-              <label className="support-form__label">
-              Tell us a little bit more about how we can help you
-              </label>
-              <TextArea className="support-form__input support-form__input__textarea" placeholder="Enter your message" name='more'/>           
-            </div>
+
+                <TextArea className="support-form__input support-form__input__textarea" placeholder="Tell us a little bit more about how we can help you" name='more'/>           
+              </div>
           </div>
           <div className="support-form__row support-form__row--modified">
             <Button className="button button--red support-form__button" >Submit</Button>
