44963b3125059187e59196020020d84b5203ea05
Harjyot Singh <harjyot7@gmail.com>
2018-01-04 14:18:42 +0000

    Changes include: 1. remove overview from nav 2. Get in touch action changed on usecases

--- src/config/navRoutes.js
+++ src/config/navRoutes.js
@@ -7,10 +7,6 @@ export const internalRoutes =  [
 ];
 
 export const navRoutes = {
-  Overview: {
-    type: 'single',
-    link: `${graknRoutes.overview}`,
-  },
   Database: {
     type: 'multi',
     links: {
--- src/templates/UseCase.js
+++ src/templates/UseCase.js
@@ -1,28 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Testimonials from 'components/Testimonials';
+import SupportFormModal from 'components/SupportFormModal';
 
-const UseCase = ({ title, text, img }) => (
-  <div className="usecase-page">
-    <section className="usecase-page__splash">
-      <div className="usecase-page__splash__text">
-        <span className="usecase-page__splash__text__header"><strong>Grakn helps</strong> every domain with complex networks of information </span>
-      </div>
-    </section>
-    <section className="usecase-page__info">
-      <div className="usecase-page__info__container container section__container">
-        <div className="usecase-page__info__details">
-          <div className="usecase-page__info__details__header">
-          {title}
+class UseCase extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      supportModal: false,
+    };
+    this.switchSupportModal = this.switchSupportModal.bind(this);
+  }
+
+  switchSupportModal() {
+    this.setState({
+      supportModal: !this.state.supportModal
+    });
+  }
+
+  render() {
+    const { title, img, text} = this.props;
+    return (
+      <div className="usecase-page">
+        <section className="usecase-page__splash">
+          <div className="usecase-page__splash__text">
+            <span className="usecase-page__splash__text__header"><strong>Grakn helps</strong> every domain with complex networks of information </span>
           </div>
-          <div className="usecase-page__info__details__text" dangerouslySetInnerHTML={{__html: text}} />
-        </div>
-        <div className="usecase-page__info__img">
-          <img src={img} alt={title} />
-        </div>
+        </section>
+        <section className="usecase-page__info">
+          <div className="usecase-page__info__container container section__container">
+            <div className="usecase-page__info__details">
+              <div className="usecase-page__info__details__header">
+              {title}
+              </div>
+              <div className="usecase-page__info__details__text" dangerouslySetInnerHTML={{__html: text}} />
+            </div>
+            <div className="usecase-page__info__img">
+              <img src={img} alt={title} />
+            </div>
+          </div>
+        </section>
+        <Testimonials buttonCallback={this.switchSupportModal} />
+        <SupportFormModal isOpen={this.state.supportModal} onClose={this.switchSupportModal}/>
       </div>
-    </section>
-    <Testimonials />
-  </div>
-);
+    )
+  }
+}
 
 export default UseCase;
