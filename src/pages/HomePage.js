5dba670b0ab902fc2e53bb1c0e2b1f7f79582a1b
Haikal Pribadi <haikal@grakn.ai>
2018-07-14 17:25:14 +0100

    changed the sub title a bit

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -32,7 +32,7 @@ class HomePage extends Component {
         <section className="home__splash">
           <div className="home__splash__text container section__container">
             <h1 className="home__splash__text__headline home__header"><strong>Building intelligent systems starts at the database</strong></h1>
-            <span className="home__splash__text__tag">Grakn is a database that turns complex data into a <u>knowlege graph</u></span>
+            <span className="home__splash__text__tag">Grakn turns complex data into a <u>knowlege graph</u></span>
             <Link to="/grakn-core" className="home__splash__text__install" onClick={() => {
               ReactGA.initialize('UA-72414051-1');
               ReactGA.event({
