fa7c1c8b49afecdb7a02bfbdb86f3c84b705cf75
sorsaffari <soroush@grakn.ai>
2019-04-09 17:55:20 +0100

    make iframe css of slack page exclussive

--- src/pages/SlackPage.js
+++ src/pages/SlackPage.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const SlackPage = () => (
-  <iframe src="https://grakn-slackin.herokuapp.com/" scrolling="no" />
+  <div className="slackPage">
+    <iframe src="https://grakn-slackin.herokuapp.com/" scrolling="no" />
+  </div>
 );
 
 export default SlackPage;
