147b8758cb56fd9344a7f2c177815409f552ea33
sorsaffari <soroush@grakn.ai>
2019-04-12 13:46:41 +0100

    debugging express.static

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
-app.use(express.static(path.join(__dirname, 'src/pages/landingpages/')));
+app.use(express.static(path.join(__dirname, '/src/pages/landingpages')));
 app.use('/', express.static(dist));
 
 const whitelist = [
