c33fb69feeeb91b34029b7ead12fdef462d013b9
sorsaffari <soroush@grakn.ai>
2019-04-16 15:28:07 +0100

    debug the last major api refactor - 3

--- api/routes/email.js
+++ api/routes/email.js
@@ -14,48 +14,48 @@ router.post(
         }).unknown()
     }),
     (req, res) => {
-        // console.log(`email/enterprise call from ${req.get('host')} - payload`, JSON.stringify(req.body));
+        console.log(`email/enterprise call from ${req.get('host')} - payload`, JSON.stringify(req.body));
 
-        // const { emailTitle, email, firstname, lastname, company, job, product, stage, aois, more } = req.body;
+        const { emailTitle, email, firstname, lastname, company, job, product, stage, aois, more } = req.body;
 
-        // const mailOptions = {
-        //     from: 'postmaster@mail.grakn.ai',
-        //     to: 'enterprise@grakn.ai',
-        //     subject: emailTitle,
-        //     replyTo: email,
-        //     text: JSON.stringify(req.body),
-        //     html:
-        //     `
-        //      <h3> ${emailTitle} </h3>
-        //      <div>Name: ${firstname || ""} ${lastname || ""}</div>
-        //      <div>Company: ${company || ""}</div>
-        //      <div>Position: ${job || ""}</div>
-        //      <div>Email: ${email}</div>
-        //      <div>Product: ${product || ""}</div>
-        //      <div>Stage of Development: ${stage || ""}</div>
-        //      <div>Areas of Interest: ${aois || ""}</div>
-        //      <div>Additional: ${more || ""}</div>
-        //     `
-        // };
+        const mailOptions = {
+            from: 'postmaster@mail.grakn.ai',
+            to: 'enterprise@grakn.ai',
+            subject: emailTitle,
+            replyTo: email,
+            text: JSON.stringify(req.body),
+            html:
+            `
+             <h3> ${emailTitle} </h3>
+             <div>Name: ${firstname || ""} ${lastname || ""}</div>
+             <div>Company: ${company || ""}</div>
+             <div>Position: ${job || ""}</div>
+             <div>Email: ${email}</div>
+             <div>Product: ${product || ""}</div>
+             <div>Stage of Development: ${stage || ""}</div>
+             <div>Areas of Interest: ${aois || ""}</div>
+             <div>Additional: ${more || ""}</div>
+            `
+        };
 
-        // const transporter = nodemailer.createTransport({
-        //     host: 'smtp.mailgun.org',
-        //     port: 587,
-        //     auth: {
-        //         user: mailOptions.from,
-        //         pass: process.env.SUPPORT_PASS
-        //     }
-        // });
+        const transporter = nodemailer.createTransport({
+            host: 'smtp.mailgun.org',
+            port: 587,
+            auth: {
+                user: mailOptions.from,
+                pass: process.env.SUPPORT_PASS
+            }
+        });
 
-        // transporter.sendMail(mailOptions, (e, info) => {
-        //     if (e) {
-        //         console.log(`Sending the email ${emailTitle} to ${mailOptions.from} failed.`, e);
-        //         res.status(500).send(JSON.stringify({msg: "Form submission failed! Try Again.."}));
-        //         return false;
-        //     }
-        //     console.log(`Sending the email [${emailTitle}] to [${mailOptions.from}] succeeded.`);
-        //     res.status(200).send(JSON.stringify({ msg: "Thank you for submitting the support form! A member of our team will be in touch shortly." }));
-        // });
+        transporter.sendMail(mailOptions, (e, info) => {
+            if (e) {
+                console.log(`Sending the email ${emailTitle} to ${mailOptions.from} failed.`, e);
+                res.status(500).send(JSON.stringify({msg: "Form submission failed! Try Again.."}));
+                return false;
+            }
+            console.log(`Sending the email [${emailTitle}] to [${mailOptions.from}] succeeded.`);
+            res.status(200).send(JSON.stringify({ msg: "Thank you for submitting the support form! A member of our team will be in touch shortly." }));
+        });
     }
 );
 
