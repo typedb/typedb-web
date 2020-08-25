b074320be67a7fb5da9d6221d1751e3bb8338fb3
daniel crowe <daniel@grakn.ai>
2020-08-25 13:19:46 +0100

    Updates the events for August (#73)
    Updated dates for existing events + added financial, rdf webinars

--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -14,6 +14,11 @@ const tags = {
   security: "#security",
   telecom: "#telecom",
   graql: "#graql",
+  masterdatamgmt: "master data management",
+  customer360: "customer 360",
+  antimoneylaundering: "anti-money laundering",
+  comparison: "comparison",
+  rdf: "rdf"
 };
 
 const types = {
@@ -52,29 +57,25 @@ const eventsData = [
     imageUrl: "src/pages/events/images/event-intro-to-kg.jpeg",
     slots: [
       {
-        date: new Date("June 04 2020 18:30"),
+        date: new Date("August 27 2020 18:30"),
         rsvpUrl: "https://zoom.us/webinar/register/WN_TRNpROP-R7mG-wkf5QjHWA",
       },
       {
-        date: new Date("June 11 2020 18:30"),
+        date: new Date("September 10 2020 18:30"),
         rsvpUrl: "https://zoom.us/webinar/register/WN_TRNpROP-R7mG-wkf5QjHWA",
       },
       {
-        date: new Date("June 18 2020 18:30"),
+        date: new Date("September 24 2020 18:30"),
         rsvpUrl: "https://zoom.us/webinar/register/WN_TRNpROP-R7mG-wkf5QjHWA",
       },
       {
-        date: new Date("June 25 2020 18:30"),
+        date: new Date("October 8 2020 18:30"),
         rsvpUrl: "https://zoom.us/webinar/register/WN_TRNpROP-R7mG-wkf5QjHWA",
       },
       {
-        date: new Date("July 02 2020 18:30"),
+        date: new Date("October 22 2020 18:30"),
         rsvpUrl: "https://zoom.us/webinar/register/WN_TRNpROP-R7mG-wkf5QjHWA",
       },
-      {
-        date: new Date("July 09 2020 18:30"),
-        rsvpUrl: "https://zoom.us/webinar/register/WN_TRNpROP-R7mG-wkf5QjHWA",
-      }
     ],
     speaker: speakers.tomasSabat
   },
@@ -82,7 +83,7 @@ const eventsData = [
   {
     title: "How can we complete a Knowledge Graph?",
     path: "/webinar-kgcn",
-    rsvpUrl: "https://us02web.zoom.us/webinar/register/WN_tgNmzvOtRza3d-K7jfi4iA",
+    rsvpUrl: "https://us02web.zoom.us/webinar/register/WN_DPpCGTP5Q7ex0kciaSvV7Q",
     tags: [tags.ml, tags.grakndev, tags.graql],
     type: types.webinar,
     description: `A Knowledge Graph is as valuable as the insights we can derive from it. So, what do we do when our Knowledge Graph doesn’t contain the answers? We need to complete it.
@@ -90,23 +91,19 @@ const eventsData = [
     We know that Grakn’s logical reasoner can help us to deduce insights. However, when our answers can’t be deduced we need to turn to statistical methods to infer new facts - making predictions inductively, by example. This could be relations, attributes or even rules.
     
     In this talk, we will delve into the advanced graph learning systems that we can construct and use on top of Grakn to create intelligent systems. This is the core of the research that we conduct at Grakn Labs - all of which is made available in KGLIB.`,
-    imageUrl: "src/pages/events/images/event-intro-to-kg.jpeg",
+    imageUrl: "src/pages/events/images/event-kgcn.jpg",
     slots: [
       {
-        date: new Date("June 09 2020 16:30"),
-        rsvpUrl: "https://us02web.zoom.us/webinar/register/WN_tgNmzvOtRza3d-K7jfi4iA",
+        date: new Date("September 09 2020 16:30"),
+        rsvpUrl: "https://us02web.zoom.us/webinar/register/WN_DPpCGTP5Q7ex0kciaSvV7Q",
       },
       {
-        date: new Date("July 15 2020 16:30"),
-        rsvpUrl: "https://us02web.zoom.us/webinar/register/WN_UpbEswCPQlCKUCYv87HlZA",
+        date: new Date("October 05 2020 16:30"),
+        rsvpUrl: "https://us02web.zoom.us/webinar/register/WN_XMoe9MA9QyOPtblokyGO0A",
       },
       {
-        date: new Date("August 11  2020 16:30"),
-        rsvpUrl: "https://us02web.zoom.us/webinar/register/WN_m9ClAIFkRDiZ7tziS1fZZA",
-      },
-      {
-        date: new Date("September 02  2020 16:30"),
-        rsvpUrl: "https://us02web.zoom.us/webinar/register/WN_m9ClAIFkRDiZ7tziS1fZZA",
+        date: new Date("November 17 2020 16:30"),
+        rsvpUrl: "https://us02web.zoom.us/webinar/register/WN_uZMOU3k-TwuUJtKaHOWj5w",
       },
     ],
     speaker: speakers.jamesFletcher
@@ -115,7 +112,7 @@ const eventsData = [
   {
     title: "Beyond Text Mining - Text Mined Knowledge Graphs",
     path: "/webinar-textmining",
-    rsvpUrl: "https://zoom.us/webinar/register/WN_2Hog2n86TnuSTwcaUk8a_A",
+    rsvpUrl: "https://us02web.zoom.us/webinar/register/WN_OLshO8mwQLazlwxvO7Lbkg",
     tags: [tags.grakndev, tags.nlp, tags.graql],
     type: types.webinar,
     description: `Text is the medium used to store the tremendous wealth of scientific knowledge regarding the world we live in. However, with its ever-increasing magnitude and throughput, analysing this unstructured data has become a tedious task. This has led to the rise of Natural Language Processing (NLP), as the go-to for examining and processing large amounts of natural language data.
@@ -123,15 +120,23 @@ const eventsData = [
     This involves the automatic extraction of structured semantic information from unstructured machine-readable text. The identification of these explicit concepts and relationships help in discovering multiple insights contained in text in a scalable and effective way.
     
     A major challenge is the mapping of unstructured information from raw texts into entities, relationships and attributes in the knowledge graph. In this talk, we demonstrate how Grakn can be used to create a text mining knowledge graph capable of modelling, storing, and exploring beneficial information extracted from medical literature.`,
-    imageUrl: "src/pages/events/images/event-intro-to-kg.jpeg",
+    imageUrl: "src/pages/events/images/event-text-mining.png",
     slots: [
       {
-        date: new Date("June 10 2020 12:30"),
-        rsvpUrl: "https://zoom.us/webinar/register/WN_2Hog2n86TnuSTwcaUk8a_A",
+        date: new Date("August 25 2020 16:30"),
+        rsvpUrl: "https://us02web.zoom.us/webinar/register/WN_OLshO8mwQLazlwxvO7Lbkg",
       },
       {
-        date: new Date("July 21 2020 18:30"),
-        rsvpUrl: "https://us02web.zoom.us/webinar/register/WN_t7ehO8K7QoC0B9l4FBRbmw",
+        date: new Date("September 15 2020 18:30"),
+        rsvpUrl: "https://us02web.zoom.us/webinar/register/WN_KLOdSPQLS4Wz46yYnRi89w",
+      },
+      {
+        date: new Date("October 6 2020 16:30"),
+        rsvpUrl: "https://us02web.zoom.us/webinar/register/WN_wGt2gKbBRhi8MJfE6G5G4A"
+      },
+      {
+        date: new Date("November 4 2020 16:30"),
+        rsvpUrl: "https://us02web.zoom.us/webinar/register/WN_jELKOqj0QcKg8JrZuXUAlQ"
       },
     ],
     speaker: speakers.tomasSabat
@@ -140,21 +145,21 @@ const eventsData = [
   {
     title: "Drug Discovery with Grakn",
     path: "/webinar-drugdiscovery",
-    rsvpUrl: "https://zoom.us/webinar/register/WN_D9mK_i3tQY2Js5dYw1kthw",
-    tags: [tags.grakndev, tags.nlp, tags.graql],
+    rsvpUrl: "https://us02web.zoom.us/webinar/register/WN_F_eqlc7qR5KjXBDrmUetkg",
+    tags: [tags.grakndev, tags.nlp, tags.lifesciences],
     type: types.webinar,
     description: `Combinatorial chemistry has produced a huge amount of chemical libraries and data banks which include prospective drugs. Despite all of this progress, the fundamental problem still remains: how do we take advantage of this data to identify the prospective nature of a compound as a vital drug? Traditional methodologies fail to provide a solution to this.
 
     Knowledge graphs, however, provide the framework which can make drug discovery much more efficient, effective and approachable. This radical advancement in technology can model biological knowledge complexity as it is found at its core. With concepts such as hyper relationships, type hierarchies, automated reasoning and analytics we can finally model, represent, and query biological knowledge at an unprecedented scale.`,
-    imageUrl: "src/pages/events/images/event-intro-to-kg.jpeg",
+    imageUrl: "src/pages/events/images/event-drug-discovery.png",
     slots: [
       {
-        date: new Date("June 16 2020 16:30"),
-        rsvpUrl: "https://zoom.us/webinar/register/WN_D9mK_i3tQY2Js5dYw1kthw",
+        date: new Date("October 7 2020 16:30"),
+        rsvpUrl: "https://us02web.zoom.us/webinar/register/WN_F_eqlc7qR5KjXBDrmUetkg",
       },
       {
-        date: new Date("July 28 2020 18:30"),
-        rsvpUrl: "https://us02web.zoom.us/webinar/register/WN_07w-i2aETACD-N2C4kWdXA",
+        date: new Date("November 9 2020 16:30"),
+        rsvpUrl: "https://us02web.zoom.us/webinar/register/WN_RN_t7I-RQiSqZ7-IH6Xrmw",
       },
     ],
     speaker: speakers.tomasSabat
@@ -163,8 +168,8 @@ const eventsData = [
   {
     title: "Beyond SQL | comparing SQL to Graql",
     path: "/webinar-sql-to-graql",
-    rsvpUrl: "https://zoom.us/webinar/register/WN_J23_-ccVQH6nYclU9jkgog",
-    tags: [tags.grakndev, tags.graql],
+    rsvpUrl: "https://us02web.zoom.us/webinar/register/WN_OTB-9fQQTRGJJHSsjXr9ZA",
+    tags: [tags.grakndev, tags.graql, tags.comparison],
     type: types.webinar,
     description: `Using SQL to query relational databases is easy. As a declarative language, it’s straightforward to write queries and build powerful applications. However, relational databases struggle when working with complex data. When querying such data in SQL, challenges especially arise in the modelling and querying of the data.
 
@@ -173,19 +178,92 @@ const eventsData = [
     Graql is the query language used in Grakn. Just as SQL is the standard query language in relational databases, Graql is Grakn’s query language. It’s a declarative language, and allows us to model, query and reason over our data.
     
     In this talk, we will look at how Graql compares to SQL. Why and when should you use Graql over SQL? How do we do outer/inner joins in Graql? We'll look at the common concepts, but mostly talk about the differences between the two.`,
-    imageUrl: "src/pages/events/images/event-intro-to-kg.jpeg",
+    imageUrl: "src/pages/events/images/event-sql.jpeg",
     slots: [
       {
-        date: new Date("June 23 2020 11:30"),
-        rsvpUrl: "https://zoom.us/webinar/register/WN_J23_-ccVQH6nYclU9jkgog",
-      },
-      {
-        date: new Date("July 14 2020 16:30"),
-        rsvpUrl: "https://us02web.zoom.us/webinar/register/WN_msz1m9ozTiC4csETWSvzPg",
-      },
-      {
-        date: new Date("September 01 2020 16:30"),
+        date: new Date("September 1 2020 16:30"),
         rsvpUrl: "https://us02web.zoom.us/webinar/register/WN_OTB-9fQQTRGJJHSsjXr9ZA",
+      },
+      {
+        date: new Date("September 29th 2020 18:30"),
+        rsvpUrl: "https://us02web.zoom.us/webinar/register/WN_-RENGqLjTZij9KZIeAkRNw",
+      },
+      {
+        date: new Date("October 15 2020 16:30"),
+        rsvpUrl: "https://us02web.zoom.us/webinar/register/WN_g3pGCdEmS1OgdMd7ov66QQ",
+      },
+      {
+        date: new Date("November 16 2020 16:30"),
+        rsvpUrl: "https://us02web.zoom.us/webinar/register/WN_axly5fllS9WBVwbE47QQ0g"
+      },
+    ],
+    speaker: speakers.tomasSabat
+  },
+
+  {
+    title: "Knowledge Graphs for Financial Services",
+    path: "/webinar-financial-services",
+    rsvpUrl: "https://us02web.zoom.us/webinar/register/WN_0baUrwvmQMawQxAmNsKjQA",
+    tags: [tags.finance, tags.masterdatamgmt,tags.customer360],
+    type: types.webinar,
+    description: `Covid has had a massive impact on the financial services industry. Existing changes in technology, regulation, and geopolitics are radically changing the data landscape. Faced with this environment, financial services firms can take full strategic advantage of the most cutting-edge data infrastructure technologies to thrive in these unprecedented times.
+
+    In this talk, we’ll explore how Grakn can be used to make the most of current challenges. We’ll explore how to unite data silos into a federated model and analyse data across an enterprise in real time, enabling use cases such as customer 360, risk & compliance and anti money laundering.`,
+    imageUrl: "src/pages/events/images/event-fin-serve.png",
+    slots: [
+      {
+        date: new Date("August 26 2020 16:30"),
+        rsvpUrl: "https://us02web.zoom.us/webinar/register/WN_0baUrwvmQMawQxAmNsKjQA",
+      },
+      {
+        date: new Date("September 8 2020 16:30"),
+        rsvpUrl: "https://us02web.zoom.us/webinar/register/WN_ycjZdfI-TKeH_YbM-PPokQ",
+      },
+      {
+        date: new Date("September 30 2020 16:30"),
+        rsvpUrl: "https://us02web.zoom.us/webinar/register/WN_jGwW9YaJQKyRNL1e6A216g",
+      },
+      {
+        date: new Date("October 14 2020 16:30"),
+        rsvpUrl: "https://us02web.zoom.us/webinar/register/WN_d8GoWQmJSbuffzQe3p4TVw"
+      },
+      {
+        date: new Date("November 3 2020 16:30"),
+        rsvpUrl: "https://us02web.zoom.us/webinar/register/WN_X46h0o3RRtuyrgzgcUdDrg"
+      },
+    ],
+    speaker: speakers.tomasSabat
+  },
+  {
+    title: "Comparing Semantic Web Technologies with Grakn",
+    path: "/webinar-rdf-to-grakn",
+    rsvpUrl: "https://us02web.zoom.us/webinar/register/WN_HY6mMf9PQ3CcH1Zy41W41g",
+    tags: [tags.comparison, tags.grakndev, tags.rdf],
+    type: types.webinar,
+    description: `Semantic Web technologies enable us to represent and query for very complex and heterogeneous datasets. We can add semantics and reason over large bodies of data on the web. However, despite a lot of educational material available, they have failed to achieve mass adoption outside academia.
+
+    Grakn works at a higher level of abstraction and enables developers to be more productive when working with complex data. Grakn is easier to learn, reducing the barrier to entry and enabling more developers to access semantic technologies. Instead of using a myriad of standards and technologies, we just use one language - Graql.
+    
+    In this talk:
+    - we will look at how Graql compares to Semantic Web standards, specifically RDF, SPARQL RDFS, OWL and SHACL.
+    - cover questions such as, how do we represent hyper-relations in Grakn? How to use rdfs:domain and rdfs:range in Grakn? And how do the modelling philosophies compare?`,
+    imageUrl: "src/pages/events/images/event-rdf.jpg",
+    slots: [
+      {
+        date: new Date("September 02 2020 16:30"),
+        rsvpUrl: "https://us02web.zoom.us/webinar/register/WN_HY6mMf9PQ3CcH1Zy41W41g",
+      },
+      {
+        date: new Date("September 23 2020 16:30"),
+        rsvpUrl: "https://us02web.zoom.us/webinar/register/WN_6hd7u1ZtQ-aGu6TDu6_UTA",
+      },
+      {
+        date: new Date("October 13 2020 16:30"),
+        rsvpUrl: "https://us02web.zoom.us/webinar/register/WN_riravNSBQhCjLSdeoD-lQg",
+      },
+      {
+        date: new Date("November 18 2020 16:30"),
+        rsvpUrl: "https://us02web.zoom.us/webinar/register/WN_GMFHykrYTFig36MFskZvhA"
       },
     ],
     speaker: speakers.tomasSabat
