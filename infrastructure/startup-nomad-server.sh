94d3f0e8a684877b75bcc5ce98c3eac8265f93ab
Rui Liu <lriuui0x0@gmail.com>
2021-04-08 11:19:53 +0100

    wip

--- infrastructure/startup-nomad-server.sh
+++ infrastructure/startup-nomad-server.sh
@@ -2,6 +2,13 @@
 
 set -ex
 
+sleep 5m
+
+FS_TYPE=$(blkid -o value -s TYPE ${persisted_disk_name})
+[ -z "$FS_TYPE" ] && sudo mkfs.ext4 ${persisted_disk_name} || true
+sudo mkdir -p ${persisted_mount_point}
+sudo mount -t ext4 ${persisted_disk_name} ${persisted_mount_point}
+
 cat > /etc/systemd/system/nomad-server.service  << EOF
 [Unit]
 Description=Nomad Server
@@ -16,11 +23,6 @@ KillMode=process
 [Install]
 WantedBy=multi-user.target
 EOF
-
-FS_TYPE=$(blkid -o value -s TYPE ${persisted_disk_name})
-[ -z "$FS_TYPE" ] && sudo mkfs.ext4 ${persisted_disk_name} || true
-sudo mkdir -p ${persisted_mount_point}
-sudo mount -t ext4 ${persisted_disk_name} ${persisted_mount_point}
 
 sudo systemctl daemon-reload
 sudo systemctl enable nomad-server
