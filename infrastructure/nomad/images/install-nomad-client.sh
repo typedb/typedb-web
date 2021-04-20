77101614b940b872a97efa8ecca7783b2e4471a2
Rui Liu <lriuui0x0@gmail.com>
2021-04-20 14:27:21 +0100

    wip

--- infrastructure/nomad/images/install-nomad-client.sh
+++ infrastructure/nomad/images/install-nomad-client.sh
@@ -5,6 +5,12 @@ set -ex
 sudo snap install jq
 sudo apt install -qy openjdk-11-jdk
 
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+
 curl -L -o cni-plugins.tgz https://github.com/containernetworking/plugins/releases/download/v0.9.1/cni-plugins-linux-amd64-v0.9.1.tgz
 sudo mkdir -p /opt/cni/bin
 sudo tar -C /opt/cni/bin -xzf cni-plugins.tgz
