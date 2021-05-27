5defc56e45c8827cf80fd8f0ebdb5a8c91afe4b3
haikalpribadi <haikal@vaticle.com>
2021-05-27 22:52:28 +0100

    Update Safety example

--- web/pages/home/example/safety-example.ts
+++ web/pages/home/example/safety-example.ts
@@ -5,7 +5,7 @@ match
 
 $a isa person, has name "Alice";
 $b isa person, has name "Bob";
-($a, $b) isa employment; # invalid relation
+($a, $b) isa employment;
 $d isa company, has name "DataCo";
 ($b, $o) isa marriage; # invalid relation
 
