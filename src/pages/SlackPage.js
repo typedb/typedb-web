11b99514c4d77069ce4d92a6aaa45ec5f9338bc5
haikalpribadi <haikal@vaticle.com>
2021-06-15 17:14:52 +0100

    Delete Old Grakn Website

--- src/pages/SlackPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const SlackPage = () => (
-  <div className="slackPage">
-    <iframe src="https://grakn-slackin.herokuapp.com/" scrolling="no" />
-  </div>
-);
-
-export default SlackPage;
