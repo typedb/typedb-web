17b2d3101c6db83be3c993f1af842c3da7201982
Alex Walker <alex@grakn.ai>
2021-06-15 18:52:41 +0100

    Add Java syntax highlighter

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -8,10 +8,12 @@ import {VaticleWebApp} from "./app";
 import smoothscroll from "smoothscroll-polyfill";
 import "smoothscroll-anchor-polyfill";
 import scrollSnapPolyfill from "css-scroll-snap-polyfill";
+import { installPrismJava } from "../common/code/prism";
 
 smoothscroll.polyfill(); // Prerequisite for css-scroll-snap-polyfill
 scrollSnapPolyfill(); // window.scrollTo({behavior: 'smooth'}) in Safari
 
 installPrismTypeQL();
+installPrismJava();
 
 ReactDOM.render(<VaticleWebApp/>, document.getElementById('root'));
