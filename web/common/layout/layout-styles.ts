2aed6f06d2283313156a5aa3a42ae046acd5231d
haikalpribadi <haikal@vaticle.com>
2021-06-24 13:52:54 +0100

    Lighter menu border colour

--- web/common/layout/layout-styles.ts
+++ web/common/layout/layout-styles.ts
@@ -4,7 +4,7 @@ import { vaticleTheme } from "../styles/theme";
 export const headerHeight = 80;
 const footerHeight = 65;
 const headerBoxShadow = "2px 2px 1px -1px rgba(0,0,0,0.2),2px 2px 3px 0px rgba(0,0,0,0.14),2px 1px 5px 0px rgba(0,0,0,0.12)";
-const borderBlack = "#08022E"; // TODO: this was copied from code-styles.ts
+const borderBlack = "#180F49"; // TODO: this was copied from code-styles.ts
 
 export const defaultLayoutStyles = makeStyles({
     main: {
