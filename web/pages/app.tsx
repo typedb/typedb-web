6893dc80bc96a833b501d3f1cf6f1ba6da2ec56c
Alex Walker <alex@grakn.ai>
2021-06-29 18:22:11 +0100

    Add cookie consent banner

--- web/pages/app.tsx
+++ web/pages/app.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import {indexStyles} from './index-styles';
-import {VaticleRouter} from "./router";
-import { vaticleMuiTheme } from "../common/styles/theme";
+import { VaticleRouter } from "./router";
+import { vaticleMuiTheme, vaticleTheme } from "../common/styles/theme";
 import {ThemeProvider} from "@material-ui/core";
+import CookieConsent from "react-cookie-consent";
 
 export const VaticleWebApp: React.FC = () => {
     const classes = indexStyles();
@@ -12,6 +13,12 @@ export const VaticleWebApp: React.FC = () => {
             <div className={classes.root}>
                 <VaticleRouter/>
             </div>
+            <CookieConsent
+                buttonStyle={{ backgroundColor: vaticleTheme.palette.green["1"], fontFamily: "Titillium Web",
+                    color: vaticleTheme.palette.purple["3"], fontSize: 16, fontWeight: 600, borderRadius: 5, }}>
+                We use cookies to improve the user experience on our website. If you want to know more about it,
+                you can read our privacy policy.
+            </CookieConsent>
         </ThemeProvider>
     );
 };
