d86260faaa24da693ffdf44b966360fadc1da3d3
haikalpribadi <haikal@vaticle.com>
2021-06-29 12:48:14 +0100

    Typo

--- web/pages/typedb/typedb-page.tsx
+++ web/pages/typedb/typedb-page.tsx
@@ -73,7 +73,7 @@ export const TypeDBPage: React.FC = () => {
                               body="In the real world, relations aren't just binary connections between two things.
                               In rich systems, we often need to capture three or more things related with each other at
                               once. Representing them as separate binary relationships would lose information. TypeDB
-                              can naturally represent arbitrary number of things as one relation."
+                              can naturally represent an arbitrary number of things as one relation."
                               button={{text: "Documentation", href: urls.docs.home}}>
                     <TypeQLExample code={ternaryRelationsExampleCode} data={ternaryRelationsExampleGraph}/>
                 </FeatureBlock>
