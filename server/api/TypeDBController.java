2347b498fc6f9f05306915c81885959cc1800a64
joshua <joshuasend1@gmail.com>
2022-02-16 20:17:29 +0000

    Add 2.6.4 to downloads

--- server/api/TypeDBController.java
+++ server/api/TypeDBController.java
@@ -8,6 +8,6 @@ import static com.vaticle.web.main.server.api.APIUtils.okResult;
 
 public class TypeDBController extends Controller {
     public Result version() {
-        return okResult(new JsonObject().add("version", "2.6.3").toString());
+        return okResult(new JsonObject().add("version", "2.6.4").toString());
     }
 }
