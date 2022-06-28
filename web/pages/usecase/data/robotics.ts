5c3406a8faaf6a671f37ba71172660e4a0123f21
Alex Walker <alex@vaticle.com>
2022-06-28 15:49:36 +0100

    Fix typos in Robotics use case

--- web/pages/usecase/data/robotics.ts
+++ web/pages/usecase/data/robotics.ts
@@ -31,16 +31,16 @@ export const roboticsData: UseCaseData = {
         },
     },
     "section3": {
-        "title": "Increase Autonomy in Your Robotic Systems",
+        "title": "Increase Autonomy in Your Robotics Systems",
         "keyPoints": [
             {
                 "title": "Improved Task Planning Efficiency",
-                "body": "TypeDB enables you to build knowledge bases that describe the structure to understand and conceptualise the task planning of your robot in a real-world setting. Using TypeDB's inference engine, your robot will be able make decisions independently for more efficient autonomous task planning.",
+                "body": "TypeDB enables you to build knowledge bases that describe the structure to understand and conceptualise the task planning of your robot in a real-world setting. Using TypeDB's inference engine, your robot will be able to make decisions independently for more efficient autonomous task planning.",
                 "icon": InferenceIcon
             },
             {
                 "title": "More Intelligent Environment Modelling",
-                "body": "As your robot navigates through the real world, it obtains sensory features and creates symbolic representations in TypeDB. By leveraging the full expressivity of the ER model, this enables TypeDB to create contextual knowledge that your robot can use to reason and make decisions independently.",
+                "body": "As your robot navigates through the real world, it obtains sensory features and creates symbolic representations in TypeDB. By leveraging the full expressivity of the entity-relationship model, this enables TypeDB to create contextual knowledge that your robot can use to reason and make decisions independently.",
                 "icon": LearningAndTrainingIcon
             },
             {
@@ -52,23 +52,23 @@ export const roboticsData: UseCaseData = {
         "keyPointPanelHeight": { desktop: 408, mobile: 344 }
     },
     "section4": {
-        "title": "Build Robotic Systems on the Shoulders of TypeDB",
+        "title": "Build Robotics Systems on the Shoulders of TypeDB",
         "keyPoints": [
             {
                 "title": "Autonomous Drones",
-                "body": "For autonomous drones to move on their own, they require to perform tasks autonomously such as environment modelling, place localisation and task planning. TypeDB becomes the unifying knowledge base that ties these systems together, enabling the drone to make decisions on its own without needing human intervention."
+                "body": "For autonomous drones to move on their own, they are required to perform tasks autonomously such as environment modelling, place localisation, and task planning. TypeDB becomes the unifying knowledge base that ties these systems together, enabling the drone to make decisions on its own without needing human intervention."
             },
             {
-                "title": "Self-driving Cars",
-                "body": "A self-driving car needs to make sense of heterogeneous data such as traffic rules, incoming sensor information, physical environments and much more. TypeDB is uniquely suited to accurately represent this level of complexity and enable the robot to leverage it for more accurate and confident autonomous decision making ([further reading](https://dzone.com/articles/using-typedb-for-self-driving-autonomous-vehicles))."
+                "title": "Self-Driving Cars",
+                "body": "A self-driving car needs to make sense of heterogeneous data such as traffic rules, incoming sensor information, physical environments, and much more. TypeDB is uniquely suited to accurately represent this level of complexity and enable the robot to leverage it for more accurate and confident autonomous decision making ([further reading](https://dzone.com/articles/using-typedb-for-self-driving-autonomous-vehicles))."
             },
             {
                 "title": "Search & Rescue Robots",
-                "body": "In real-world scenarios, new facts are constantly presented to the robot during active search and rescue missions which require the robot to have a database that allows it to update its underlying knowledge on the fly. TypeDB makes this possible in a suitable and automated way. Learn more in [this article](https://hackernoon.com/object-recognition-with-spot-from-boston-robotics-9g2m347j) and [presentation](https://www.youtube.com/watch?v=3oAu4_bxjAg) from [TNO](https://www.tno.nl/)."
+                "body": "In real-world scenarios, new facts are constantly presented to the robot during active search and rescue missions, which require the robot to have a database that allows it to update its underlying knowledge on the fly. TypeDB makes this possible in a suitable and automated way. Learn more in [this article](https://hackernoon.com/object-recognition-with-spot-from-boston-robotics-9g2m347j) and [presentation](https://www.youtube.com/watch?v=3oAu4_bxjAg) from [TNO](https://www.tno.nl/)."
             }
         ],
         "body": [
-            "Whatever type of Robotic System you are building, TypeDB is the perfect solution to serve as the knowledge base to move a step closer to autonomy in robotics. TypeDB's rich and logical type system enables you to build powerful ontologies that aggregate disparate sources of data and infer new knowledge through its reasoning engine.",
+            "Whatever type of Robotics System you are building, TypeDB is the perfect solution to serve as the knowledge base to move a step closer to autonomy in robotics. TypeDB's rich and logical type system enables you to build powerful ontologies that aggregate disparate sources of data and infer new knowledge through its reasoning engine.",
             "We provide several resources to help you get started with TypeDB. A good place to begin is this [TypeDB introductory video](https://www.youtube.com/watch?v=e0lmTSb-rzY) and our 3-part [TypeDB Academy](https://www.youtube.com/watch?v=vfSAgFayLnk&list=PLtEF8_xCPklasC8w3Il718lrpD32MinC6) training series. Make sure not to miss our [Quickstart](https://docs.vaticle.com/docs/general/quickstart) tutorial, and we also have several example knowledge graphs available for you to play with on Github: [cybersecurity](https://github.com/typedb-osi/typedb-cti), [life sciences](https://github.com/typedb-osi/typedb-bio) and [offshore leaks](https://github.com/typedb-osi/typedb-offshoreleaks)."
         ]
     },
