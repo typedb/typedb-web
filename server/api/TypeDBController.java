e33ce73755687b7f4859403629cfcbe6cddffc38
James Williams <james.williams@vaticle.com>
2022-11-25 20:06:25 +0000

    Version 2.14 update (#191)
    ## What is the goal of this PR?
    
    We've updated our main site to reflect the release of TypeDB 2.14.0.
    
    ## What are the changes implemented in this PR?
    
    We've updated the API, dates of release and versions for TypeDB and
    TypeDB Studio.

--- server/api/TypeDBController.java
+++ server/api/TypeDBController.java
@@ -8,6 +8,6 @@ import static com.vaticle.web.main.server.api.APIUtils.okResult;
 
 public class TypeDBController extends Controller {
     public Result version() {
-        return okResult(new JsonObject().add("version", "2.13.0").toString());
+        return okResult(new JsonObject().add("version", "2.14.1").toString());
     }
 }
