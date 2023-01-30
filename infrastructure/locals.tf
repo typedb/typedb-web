1c4f6e1198b7fecccefd29e16755a4d4779b9bfc
Alex Walker <alex@vaticle.com>
2023-01-30 18:20:22 +0000

    CI automation for build, Docker + Helm

--- infrastructure/locals.tf
+++ infrastructure/locals.tf
@@ -1,6 +1,6 @@
 locals {
   workload_id_namespace = "kubernetes-sa"
-  workload_id_kubernetes_sa = "gcp-service-account-2"
+  workload_id_kubernetes_sa = "gcp-service-account"
   config_output_file = "config.properties"
   docker_version = 1
 }
