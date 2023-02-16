8f1120499a2dbdad453b0fc42233d10242cff5d1
Alex Walker <alex@vaticle.com>
2023-02-16 13:24:10 +0000

    Use GCS backend

--- infrastructure/dependencies.tf
+++ infrastructure/dependencies.tf
@@ -1,3 +1,9 @@
 terraform {
+  backend "gcs" {
+    bucket = "vaticle-web-prod-terraform-state"
+    prefix = "terraform/applications/typedb-web"
+    credentials = "credentials/gcp/prod/credentials.json"
+  }
+
   required_version = ">= 1.0.11, < 2.0.0"
 }
