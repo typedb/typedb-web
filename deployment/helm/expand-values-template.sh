546b0716517d787cf97d0955a59041f8a0b585f4
Alex Walker <alex@vaticle.com>
2023-02-23 17:37:13 +0000

    Fix broken CI scripts

--- deployment/helm/expand-values-template.sh
+++ deployment/helm/expand-values-template.sh
@@ -1,3 +1,3 @@
-set -ex
+set -eux
 
-sed -i -e "s/{COMMIT_SHA}/$(git rev-parse HEAD)/g" deployment/helm/values.yaml
+sed -i -e "s/{COMMIT_SHA}/$(git rev-parse HEAD)/g" $BUILD_WORKSPACE_DIRECTORY/deployment/helm/values.yaml
