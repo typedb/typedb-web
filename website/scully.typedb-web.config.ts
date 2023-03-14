6bac339af6145734d4efef97dbdfc6d5c8cad9e4
Alex Walker <alex@vaticle.com>
2023-03-14 10:13:09 +0000

    In Scully, prerender routes loaded from Sanity

--- website/scully.typedb-web.config.ts
+++ website/scully.typedb-web.config.ts
@@ -1,9 +1,13 @@
 import { ScullyConfig } from "@scullyio/scully";
 import "@scullyio/scully-plugin-puppeteer";
+import { pageRoutes } from "./scully/plugins/plugins";
+
 
 export const config: ScullyConfig = {
     projectRoot: "./src",
     projectName: "typedb-web",
     outDir: "./dist/static",
-    routes: {},
+    routes: {
+        "/:route": { type: pageRoutes },
+    },
 };
--- website/scully/plugins/plugins.ts
+++ website/scully/plugins/plugins.ts
@@ -1,11 +1,20 @@
-import { registerPlugin, getPluginConfig } from '@scullyio/scully';
-
-export const myPlugin = 'myPlugin';
-
-const myFunctionPlugin = async (html: string): Promise<string> => {
-    return html;
-};
+import { registerPlugin, getPluginConfig, HandledRoute } from '@scullyio/scully';
+import axios from "axios";
+import { SANITY_TOKEN } from "../../src/service/credentials/token";
 
 const validator = async () => [];
 
-registerPlugin('render', myPlugin, myFunctionPlugin, validator);
+export const pageRoutes = "pageRoutes";
+
+const SANITY_PROJECT_ID = "xndl14mc";
+const SANITY_URL = `https://${SANITY_PROJECT_ID}.api.sanity.io/v2021-10-21/data/query/production`;
+
+async function pageRoutesPlugin(route: string, config = {}): Promise<HandledRoute[]> {
+    const { data } = await axios.get<{ result: string[] }>(SANITY_URL, {
+        headers: { "Authorization": `Bearer ${SANITY_TOKEN}` },
+        params: { "query": "*[_type == 'page'].route.current" },
+    });
+    return data.result.map(x => ({ route: x }));
+}
+
+registerPlugin("router", pageRoutes, pageRoutesPlugin, validator);
