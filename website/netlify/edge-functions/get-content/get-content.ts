70bf5b070e6e14a41087939a0c9122412b9125d7
Alex Walker <alex@vaticle.com>
2023-05-30 14:28:53 -0700

    Try fixing netlify edge fn again

--- website/netlify/edge-functions/get-content/get-content.ts
+++ website/netlify/edge-functions/get-content/get-content.ts
@@ -1,10 +1,23 @@
 import type { Context } from "https://edge.netlify.com";
 
 export default async (request: Request, context: Context) => {
-    return await fetch(`${Netlify.env.get("SANITY_URL")}/v2021-10-21/data/query/production?query=*[!(_type match 'system.**')]`, {
-        method: "GET",
-        headers: {
-            "Authorization": `Bearer ${Netlify.env.get("SANITY_TOKEN")}`
-        }
-    });
+    const queryString = request.url.includes("?") ? request.url.split("?")[1] : null;
+    const params = queryString ? new URLSearchParams(queryString) : null;
+    const draft = params?.get("draft") === "true";
+
+    if (draft) {
+        return await fetch(`${Netlify.env.get("SANITY_URL")}/v2021-10-21/data/query/production?query=*[!(_type match 'system.**')]`, {
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${Netlify.env.get("SANITY_TOKEN")}`
+            }
+        });
+    } else {
+        return await fetch(`${Netlify.env.get("SANITY_URL")}/v2021-10-21/data/query/production?query=*[!(_id in path('drafts.**')) && !(_type match 'system.**')]`, {
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${Netlify.env.get("SANITY_TOKEN")}`
+            }
+        });
+    }
 };
