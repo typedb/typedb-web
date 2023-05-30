f39986decd3ea2574b2d05bb56f2f84f3f856e15
Alex Walker <alex@vaticle.com>
2023-05-30 14:24:12 -0700

    Try to fix get-content edge fn

--- website/netlify/edge-functions/get-content/get-content.ts
+++ website/netlify/edge-functions/get-content/get-content.ts
@@ -1,10 +1,7 @@
 import type { Context } from "https://edge.netlify.com";
 
 export default async (request: Request, context: Context) => {
-    const queryString = request.url.includes("?") ? request.url.split("?")[1] : null;
-    const params = queryString ? new URLSearchParams(queryString) : null;
-    const query = params?.get("draft") === "true" ? "*[!(_type match 'system.**')]" : "*[!(_id in path('drafts.**')) && !(_type match 'system.**')]";
-    return await fetch(`${Netlify.env.get("SANITY_URL")}/v2021-10-21/data/query/production?query=${query}`, {
+    return await fetch(`${Netlify.env.get("SANITY_URL")}/v2021-10-21/data/query/production?query=*[!(_type match 'system.**')]`, {
         method: "GET",
         headers: {
             "Authorization": `Bearer ${Netlify.env.get("SANITY_TOKEN")}`
