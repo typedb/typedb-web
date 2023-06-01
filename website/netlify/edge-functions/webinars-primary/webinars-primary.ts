1505d891e5aaa604027e50c9f7d598c6fa445a4d
Alex Walker <alex@vaticle.com>
2023-06-01 13:55:27 -0700

    Add timing to edge fn

--- website/netlify/edge-functions/webinars-primary/webinars-primary.ts
+++ website/netlify/edge-functions/webinars-primary/webinars-primary.ts
@@ -4,6 +4,7 @@ const AIRMEET_API_URL = `https://api-gateway.airmeet.com/prod`;
 const internalServerError = () => new Response(null, { status: 500, headers: { "Access-Control-Allow-Origin": "*" } });
 
 async function fetchSession(airmeet: any, token: string): Promise<any> {
+    let startTime = Date.now();
     const sessionsResponse = await fetch(`${AIRMEET_API_URL}/airmeet/${airmeet.uid}/info`, {
         method: "GET",
         headers: {
@@ -15,6 +16,8 @@ async function fetchSession(airmeet: any, token: string): Promise<any> {
         throw internalServerError();
     }
     const sessionsResponseBody = await sessionsResponse.json();
+    let endTime = Date.now();
+    console.log(`GET /airmeet/${airmeet.uid}/info: completed in ${endTime - startTime}ms`);
     return { airmeet: airmeet, session: sessionsResponseBody.sessions[0] };
 }
 
@@ -30,6 +33,7 @@ export default async (request: Request, context: Context) => {
 
     const [accessKey, secretKey] = [Netlify.env.get("AIRMEET_API_ACCESS_KEY"), Netlify.env.get("AIRMEET_API_SECRET_KEY")];
     console.log([accessKey, secretKey]);
+    let startTime = Date.now();
     const authResponse = await fetch(`${AIRMEET_API_URL}/auth`, {
         method: "POST",
         headers: {
@@ -43,7 +47,10 @@ export default async (request: Request, context: Context) => {
     }
     const authResponseBody = await authResponse.json();
     const token = authResponseBody.token;
+    let endTime = Date.now();
+    console.log("POST /auth: completed in " + (endTime - startTime) + "ms");
 
+    startTime = Date.now();
     const airmeetsResponse = await fetch(`${AIRMEET_API_URL}/airmeets`, {
         method: "GET",
         headers: {
@@ -57,6 +64,8 @@ export default async (request: Request, context: Context) => {
     const airmeetsResponseBody = await airmeetsResponse.json();
     console.log(airmeetsResponseBody);
     const airmeets: any[] = airmeetsResponseBody.data;
+    endTime = Date.now();
+    console.log("GET /airmeets: completed in " + (endTime - startTime) + "ms");
 
     let webinars;
     try {
