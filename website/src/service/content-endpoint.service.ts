7f16e8516406aa9ca1d9e2e448ecb54174c13f8b
Alex Walker <alex@vaticle.com>
2023-05-31 18:51:04 -0700

    Local and dev environments now read draft content

--- website/src/service/content-endpoint.service.ts
+++ website/src/service/content-endpoint.service.ts
@@ -5,6 +5,8 @@ import { SanityDocument } from "@sanity/types";
 const SANITY_URL = "https://xndl14mc.api.sanity.io/";
 const SANITY_QUERY_STRING = "*[!(_type match 'system.**')]";
 const SANITY_QUERY_URL = `${SANITY_URL}/v2021-10-21/data/query/production?query=${SANITY_QUERY_STRING}`;
+// Read-only API token that must be provided to read draft content
+const SANITY_TOKEN = "skIRNgnaiMLWn9XUwl20yvPaUODDE4P6kNWiRicQEthG2J4wvcCA1vRaCkTC9y4SChNzoq8BAw2vRuDEKvXRayMbVgUFsuER7otBti0zDzDk6mrEPze4oDfEPYyiw9eklL352jwFXVELvHNESrvkRiAm5IDxECjN3aYM3JjNH7bWbp5czrw3";
 
 /**
  * When running `ng build` (e.g. in production), this file is replaced with `content-endpoint.service.prod.ts`.
@@ -16,6 +18,10 @@ export class ContentEndpointService {
     constructor(private http: HttpClient) {}
 
     getContent() {
-        return this.http.get<{ result: SanityDocument[] }>(SANITY_QUERY_URL);
+        return this.http.get<{ result: SanityDocument[] }>(SANITY_QUERY_URL, {
+            headers: {
+                "Authorization": `Bearer ${SANITY_TOKEN}`,
+            }
+        });
     }
 }
