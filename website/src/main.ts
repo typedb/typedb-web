3e9a6736a1029d1811cce223663b0e0307be8a77
Alex Walker <alex@vaticle.com>
2023-06-08 11:16:49 +0100

    Syntax highlighting for all relevant languages

--- website/src/main.ts
+++ website/src/main.ts
@@ -1,9 +1,15 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { installPrismTypeQL } from "./prism-typeql";
 
 import { WebsiteModule } from './website.module';
 import { environment } from './environment/environment';
+
+import { installPrismTypeQL } from "./prism-typeql";
+import "prismjs/components/prism-java";
+import "prismjs/components/prism-python";
+import "prismjs/components/prism-rust";
+import "prismjs/components/prism-sql";
+import "prismjs/components/prism-typescript";
 
 if (environment.production) {
     enableProdMode();
