07565ab23dc1ba8c2aa6511797cbfa6161a1f30f
Bartosz Wasiluk <b.wasiluk@codeandpepper.com>
2023-07-17 14:11:54 +0200

    Formats files

--- website/src/framework/technicolor-block/technicolor-block.component.ts
+++ website/src/framework/technicolor-block/technicolor-block.component.ts
@@ -18,44 +18,62 @@ export class TechnicolorBlockComponent {
 
     get graphicLineColorClass(): string {
         switch (this.index % 3) {
-            case 0: return "tb-graphic-line-green";
-            case 1: return "tb-graphic-line-pink";
-            case 2: return "tb-graphic-line-yellow";
-            default: throw "Unreachable code";
+            case 0:
+                return "tb-graphic-line-green";
+            case 1:
+                return "tb-graphic-line-pink";
+            case 2:
+                return "tb-graphic-line-yellow";
+            default:
+                throw "Unreachable code";
         }
     }
 
     get graphicIconBgColorClass(): string {
         switch (this.index % 3) {
-            case 0: return "tb-graphic-icon-bg-green";
-            case 1: return "tb-graphic-icon-bg-pink";
-            case 2: return "tb-graphic-icon-bg-yellow";
-            default: throw "Unreachable code";
+            case 0:
+                return "tb-graphic-icon-bg-green";
+            case 1:
+                return "tb-graphic-icon-bg-pink";
+            case 2:
+                return "tb-graphic-icon-bg-yellow";
+            default:
+                throw "Unreachable code";
         }
     }
 
     get themeColorHex(): string {
         switch (this.index % 3) {
-            case 0: return "#02DAC9"; // TODO: parametrise
-            case 1: return "#FF87DC";
-            case 2: return "#FFE49E";
-            default: throw "Unreachable code";
+            case 0:
+                return "#02DAC9"; // TODO: parametrise
+            case 1:
+                return "#FF87DC";
+            case 2:
+                return "#FFE49E";
+            default:
+                throw "Unreachable code";
         }
     }
 
     get titleClass(): string {
         switch (this.size) {
-            case "large": return "text-xxl";
-            case "medium": return "text-xl";
-            case "small": return "text-l";
+            case "large":
+                return "text-xxl";
+            case "medium":
+                return "text-xl";
+            case "small":
+                return "text-l";
         }
     }
 
     get bodyClass(): string {
         switch (this.size) {
-            case "large": return "text-margin-l text-l";
-            case "medium": return "text-margin-l text-m text-xxs-mobile";
-            case "small": return "text-margin-l text-s";
+            case "large":
+                return "text-margin-l text-l";
+            case "medium":
+                return "text-margin-l text-m text-xxs-mobile";
+            case "small":
+                return "text-margin-l text-s";
         }
     }
 }
