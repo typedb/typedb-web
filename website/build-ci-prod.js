8e8b0fc643d26cbca129b29111b20ee3211cb7ed
Alex Walker <alex@vaticle.com>
2023-07-17 09:12:41 -0700

    Fail the build if Scully fails to create data.json for any route

---
+++ website/build-ci-prod.js
@@ -0,0 +1,17 @@
+const { exec } = require("child_process");
+
+exec("OUT_DIR=./dist/static npx scully --scanRoutes --noPrompt", (err, stdout, stderr) => {
+    console.log(stdout);
+    console.log(stderr);
+    if (err) {
+        console.log(`ERROR: ${err}`);
+        process.exit(1);
+    }
+    const lines = stdout.split("\n");
+    const brokenRender = lines.find(line => line.includes("rendered into") && !line.includes("data.json"));
+    if (brokenRender) {
+        const brokenRoute = brokenRender.match("Route \"(.*)\" rendered into ")[1];
+        console.error(`ERROR: Scully failed to prerender the route: "${brokenRoute}" ("data.json" was not generated)`);
+        process.exit(1);
+    }
+});
