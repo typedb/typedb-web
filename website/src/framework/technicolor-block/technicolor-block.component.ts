c5f6862b275a334e938b8f49ba73e9dd97083214
BartoszWasiluk <b.wasiluk@codeandpepper.com>
2023-07-17 17:30:15 +0200

    Tablet design for home page (#213)
    ## What is the goal of this PR?
    
    Components used on Home Page were not adjusted to the tablet design.
    Tablet designs are now implented. Components does not overflow screen
    size.
    
    ## What are the changes implemented in this PR?
    
    - added new font size combinations,
    - adjusted style in home page,
    - changed classes and styles used in multiple framework components
    related to home page.

--- website/src/framework/technicolor-block/technicolor-block.component.ts
+++ website/src/framework/technicolor-block/technicolor-block.component.ts
@@ -18,44 +18,62 @@ export class TechnicolorBlockComponent {
 
     get graphicLineColorClass(): string {
         switch (this.index % 3) {
-            case 0: return "tb-graphic-line-green";
-            case 1: return "tb-graphic-line-pink";
-            case 2: return "tb-graphic-line-yellow";
-            default: throw "Unreachable code";
+            case 0:
+                return "tb-graphic-line-green";
+            case 1:
+                return "tb-graphic-line-pink";
+            case 2:
+                return "tb-graphic-line-yellow";
+            default:
+                throw "Unreachable code";
         }
     }
 
     get graphicIconBgColorClass(): string {
         switch (this.index % 3) {
-            case 0: return "tb-graphic-icon-bg-green";
-            case 1: return "tb-graphic-icon-bg-pink";
-            case 2: return "tb-graphic-icon-bg-yellow";
-            default: throw "Unreachable code";
+            case 0:
+                return "tb-graphic-icon-bg-green";
+            case 1:
+                return "tb-graphic-icon-bg-pink";
+            case 2:
+                return "tb-graphic-icon-bg-yellow";
+            default:
+                throw "Unreachable code";
         }
     }
 
     get themeColorHex(): string {
         switch (this.index % 3) {
-            case 0: return "#02DAC9"; // TODO: parametrise
-            case 1: return "#FF87DC";
-            case 2: return "#FFE49E";
-            default: throw "Unreachable code";
+            case 0:
+                return "#02DAC9"; // TODO: parametrise
+            case 1:
+                return "#FF87DC";
+            case 2:
+                return "#FFE49E";
+            default:
+                throw "Unreachable code";
         }
     }
 
     get titleClass(): string {
         switch (this.size) {
-            case "large": return "text-xxl";
-            case "medium": return "text-xl";
-            case "small": return "text-l";
+            case "large":
+                return "text-56-64 text-48-60-tablet text-34-48-mobile";
+            case "medium":
+                return "text-40-54 text-34-48-tablet text-22-33-mobile";
+            case "small":
+                return "text-l";
         }
     }
 
     get bodyClass(): string {
         switch (this.size) {
-            case "large": return "text-margin-l text-l";
-            case "medium": return "text-margin-l text-m text-xxs-mobile";
-            case "small": return "text-margin-l text-s";
+            case "large":
+                return "tb-text-l text-32-44 text-24-37-tablet text-16-24-mobile";
+            case "medium":
+                return "tb-text-m text-24-32 text-20-32-tablet text-14-21-mobile";
+            case "small":
+                return "text-s";
         }
     }
 }
