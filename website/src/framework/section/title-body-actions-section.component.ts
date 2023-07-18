c84a2a90d2add475805468c63452bff834893d20
Bartosz Wasiluk <b.wasiluk@codeandpepper.com>
2023-07-18 17:42:32 +0200

    Formats files

--- website/src/framework/section/title-body-actions-section.component.ts
+++ website/src/framework/section/title-body-actions-section.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input } from "@angular/core";
-import { LinkButton, ParagraphWithHighlights, RichText } from "typedb-web-schema";
+import {
+    LinkButton,
+    ParagraphWithHighlights,
+    RichText,
+} from "typedb-web-schema";
 
 @Component({
     selector: "td-title-body-actions-section",
@@ -13,20 +17,26 @@ export class TitleBodyActionsSectionComponent {
     @Input() isIntroSection = false;
 
     get sectionClasses(): string {
-        return this.isIntroSection ? "page-section page-intro-section" : "page-section";
+        return this.isIntroSection
+            ? "page-section page-intro-section"
+            : "page-section";
     }
 
     get titleClass(): string {
         switch (this.size) {
-            case "l": return "text-xxl";
-            case "m": return "text-xl";
+            case "l":
+                return "text-xxl";
+            case "m":
+                return "text-xl";
         }
     }
 
     get bodyClasses(): string {
         switch (this.size) {
-            case "l": return "narrow-section subtitle-l text-l text-xs-mobile";
-            case "m": return "narrow-section subtitle text-m";
+            case "l":
+                return "narrow-section subtitle-l text-l text-xs-mobile";
+            case "m":
+                return "narrow-section subtitle text-m";
         }
     }
 }
