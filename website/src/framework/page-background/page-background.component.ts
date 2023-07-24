4a8aec3f1781e52e31c87eb34e122783b840444d
Bartosz Wasiluk <b.wasiluk@codeandpepper.com>
2023-07-24 11:46:51 +0200

    Speeds up background on bottom

--- website/src/framework/page-background/page-background.component.ts
+++ website/src/framework/page-background/page-background.component.ts
@@ -1,4 +1,3 @@
-import { BreakpointObserver, BreakpointState } from "@angular/cdk/layout";
 import {
     AfterViewInit,
     Component,
@@ -8,9 +7,6 @@ import {
     OnDestroy,
     ViewChild,
 } from "@angular/core";
-import { Subscription, map } from "rxjs";
-
-type BackgroundSize = "mobile" | "tablet" | "desktop";
 
 @Component({
     selector: "[tdPageBackground]",
@@ -20,40 +16,35 @@ type BackgroundSize = "mobile" | "tablet" | "desktop";
 export class PageBackgroundComponent implements OnDestroy, AfterViewInit {
     @Input("nebula") nebula?: string;
     @Input("planet") planet?: string;
+
     @ViewChild("nebulaRef") nebulaRef!: ElementRef<HTMLDivElement>;
     @ViewChild("planetRef") planetRef!: ElementRef<HTMLDivElement>;
 
-    private breakpointSub = Subscription.EMPTY;
     private removeListener = () => {};
 
-    private readonly breakpoints = {
-        tablet: "(max-width:1024px)",
-        mobile: "(max-width:375px)",
-    };
-    private readonly offsetStart = 300;
+    private readonly topOffset = 300;
+    private readonly bottomOffset = 300;
     private readonly spaceSpeed = 0.2;
 
     constructor(
-        private breakpointObserver: BreakpointObserver,
         private elementRef: ElementRef<HTMLElement>,
-        private ngZone: NgZone,
+        private ngZone: NgZone
     ) {}
 
     ngAfterViewInit(): void {
         this.ngZone.runOutsideAngular(() => {
-            this.breakpointSub = this.breakpointObserver
-                .observe(Object.values(this.breakpoints))
-                .pipe(map((value) => this.getActiveScreen(value)))
-                .subscribe((size) => {
-                    this.elementRef.nativeElement.style.backgroundImage = `url("/assets/background/deep-space.png")`;
-                    this.planetRef.nativeElement.style.backgroundImage = `url("/assets/background/desktop/planet-${this.planet}.png")`;
-                });
-
             const handleScroll = () => {
+                const topScrolled = Math.min(this.topOffset, window.scrollY);
+                const bottomScrolled = Math.max(
+                    0,
+                    this.bottomOffset +
+                        window.innerHeight +
+                        window.scrollY -
+                        this.elementRef.nativeElement.clientHeight
+                );
                 const distance =
-                    window.scrollY * this.spaceSpeed +
-                    (1 - this.spaceSpeed) *
-                        Math.min(this.offsetStart, window.scrollY);
+                    this.spaceSpeed * window.scrollY +
+                    (1 - this.spaceSpeed) * (topScrolled + bottomScrolled);
 
                 this.elementRef.nativeElement.style.backgroundPositionY = `${-distance}px`;
             };
@@ -68,22 +59,14 @@ export class PageBackgroundComponent implements OnDestroy, AfterViewInit {
     }
 
     ngOnDestroy(): void {
-        this.breakpointSub.unsubscribe();
         this.removeListener();
     }
 
-    getNebulaImage(): string {
-        return `url("/assets/background/nebula-${this.nebula}.png")`;
+    getNebulaSrc(): string {
+        return `/assets/background/nebula-${this.nebula}.webp`;
     }
 
-    private getActiveScreen(state: BreakpointState): BackgroundSize {
-        switch (true) {
-            case state.breakpoints[this.breakpoints.mobile]:
-                return "mobile";
-            case state.breakpoints[this.breakpoints.tablet]:
-                return "tablet";
-            default:
-                return "desktop";
-        }
+    getPlanetSrc(): string {
+        return `/assets/background/planet-${this.planet}.webp`;
     }
 }
