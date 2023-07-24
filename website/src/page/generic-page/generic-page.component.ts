b0c76758871bb1cebf57e1f70e9f8d0aa9807175
Bartosz Wasiluk <b.wasiluk@codeandpepper.com>
2023-07-24 15:42:41 +0200

    Moves article-content from article to its content

--- website/src/page/generic-page/generic-page.component.ts
+++ website/src/page/generic-page/generic-page.component.ts
@@ -1,11 +1,6 @@
 import { Component, Input, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
-import {
-    GenericPage,
-    SanityGenericPage,
-    TechnicolorBlock,
-    TitleBodyIllustrationSection,
-} from "typedb-web-schema";
+import { GenericPage, SanityGenericPage, TechnicolorBlock, TitleBodyIllustrationSection } from "typedb-web-schema";
 import { ContentService } from "../../service/content.service";
 import { Title } from "@angular/platform-browser";
 import { AnalyticsService } from "../../service/analytics.service";
@@ -25,15 +20,13 @@ export class GenericPageComponent implements OnInit {
         private contentService: ContentService,
         private _title: Title,
         private _analytics: AnalyticsService,
-        private _idleMonitor: IdleMonitorService,
+        private _idleMonitor: IdleMonitorService
     ) {}
 
     ngOnInit() {
         this.contentService.data.subscribe((data) => {
             this._activatedRoute.data.subscribe((routeData) => {
-                const sanityCloudPage = data.getDocumentByID(
-                    routeData["documentID"],
-                ) as SanityGenericPage;
+                const sanityCloudPage = data.getDocumentByID(routeData["documentID"]) as SanityGenericPage;
                 if (sanityCloudPage) {
                     this.page = new GenericPage(sanityCloudPage, data);
                     this._title.setTitle(`${this.page.title} - TypeDB`);
@@ -49,9 +42,7 @@ export class GenericPageComponent implements OnInit {
     }
 
     get articleClass(): string {
-        return this.router.url.includes("cloud")
-            ? "article-content planet-blue-pink"
-            : "article-content planet-pink";
+        return this.router.url.includes("cloud") ? "planet-blue-pink" : "planet-pink";
     }
 }
 
