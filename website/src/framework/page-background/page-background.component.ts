76da2027888964ba6f5f446652fdddbaebdb9729
BartoszWasiluk <b.wasiluk@codeandpepper.com>
2023-07-25 14:28:43 +0200

    Parallax background on home page (#218)
    ## What is the goal of this PR?
    
    Page backgrounds used to move with content.
    Now speed of stars background can be specified in the code.
    There is offset from top and bottom at which stars background moves with
    the content and other parts of background.
    Backgrounds now use more optimized `.webp` format with srcset to target
    smaller background files.
    
    ## What are the changes implemented in this PR?
    
    - added background assets and organised them into breakpoints
    directories,
    - added component responsible for displaying page background,
    - implemented background component on home page,
    - added article content class to use more precise selectors.

---
+++ website/src/framework/page-background/page-background.component.ts
@@ -0,0 +1,55 @@
+import { AfterViewInit, Component, ElementRef, Input, NgZone, OnDestroy } from "@angular/core";
+
+@Component({
+    selector: "[tdPageBackground]",
+    templateUrl: "page-background.component.html",
+    styleUrls: ["page-background.component.scss"],
+})
+export class PageBackgroundComponent implements OnDestroy, AfterViewInit {
+    @Input("nebula") nebula?: "home";
+    @Input("planet") planet?: "green";
+
+    private removeListener = () => {};
+
+    private readonly topOffset = 300;
+    private readonly bottomOffset = 300;
+    private readonly spaceSpeed = 0.2;
+
+    constructor(private elementRef: ElementRef<HTMLElement>, private ngZone: NgZone) {}
+
+    ngAfterViewInit(): void {
+        this.ngZone.runOutsideAngular(() => {
+            const handleScroll = () => {
+                const topScrolled = Math.min(this.topOffset, window.scrollY);
+                const bottomScrolled = Math.max(
+                    0,
+                    this.bottomOffset + window.innerHeight + window.scrollY - this.elementRef.nativeElement.clientHeight
+                );
+                const distance =
+                    this.spaceSpeed * window.scrollY + (1 - this.spaceSpeed) * (topScrolled + bottomScrolled);
+
+                this.elementRef.nativeElement.style.backgroundPositionY = `${-distance}px`;
+            };
+
+            this.removeListener = () => window.removeEventListener("scroll", handleScroll);
+
+            window.addEventListener("scroll", handleScroll, { passive: true });
+
+            handleScroll();
+        });
+    }
+
+    ngOnDestroy(): void {
+        this.removeListener();
+    }
+
+    getNebulaSrc(size: "desktop" | "tablet" | "mobile", density2x?: boolean): string {
+        const densityPart = density2x ? `-2x` : "";
+        return `/assets/background/${size}/nebula-${this.nebula}${densityPart}.webp`;
+    }
+
+    getPlanetSrc(size: "desktop" | "tablet" | "mobile", density2x?: boolean): string {
+        const densityPart = density2x ? `-2x` : "";
+        return `/assets/background/${size}/planet-${this.planet}${densityPart}.webp`;
+    }
+}
