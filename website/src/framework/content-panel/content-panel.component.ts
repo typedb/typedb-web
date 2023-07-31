e4dffa9ed9d0875e848ec045ec56ec7e6932c767
BartoszWasiluk <b.wasiluk@codeandpepper.com>
2023-07-31 17:49:28 +0200

    Standardise image aspect ratios (#227)
    ## What is the goal of this PR?
    
    There was a lot of variation in image aspect ratios across the site.
    They are not conforming to any particular standard.
    
    Now images use one of few ratios:
    - 1:1 (person headshots)
    - 16:9 (featured white paper on desktop and tablet, webinars, captioned
    illustration)
    - 3:2 (content panel illustrations, secondary white-papers)
    - A4 (white paper details image,  featured white paper on mobile).
    If original image has other ratio then it is resized to cover whole
    container and hide overflowing parts.
    
    ## What are the changes implemented in this PR?
    
    - removed unused content-panel-grid,
    - created new ratio-container component,
    - implemented new ratio container for applicable images.

--- website/src/framework/content-panel/content-panel.component.ts
+++ website/src/framework/content-panel/content-panel.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, OnDestroy, OnInit } from "@angular/core";
-import { Subscription } from "rxjs";
+import { Component, Input } from "@angular/core";
+import { Observable } from "rxjs";
 import { ContentTextPanel } from "typedb-web-schema";
 import { MediaQueryService } from "../../service/media-query.service";
 
@@ -8,27 +8,13 @@ import { MediaQueryService } from "../../service/media-query.service";
     templateUrl: "content-panel.component.html",
     styleUrls: ["content-panel.component.scss"],
 })
-export class ContentPanelComponent implements OnInit, OnDestroy {
+export class ContentPanelComponent {
     @Input() hidden?: boolean;
     @Input() panel!: ContentTextPanel;
-    @Input() orientation: "landscape" | "portrait" = "landscape";
-    @Input() textFirst?: boolean;
-    @Input() size: "m" | "l" = "l";
-    @Input() position: "embedded" | "standalone" = "embedded";
-    isMobile = false;
+    isMobile$: Observable<boolean>;
 
-    private mediaQuerySubscription = Subscription.EMPTY;
-
-    constructor(private _mediaQuery: MediaQueryService) {}
-
-    ngOnInit() {
-        this.mediaQuerySubscription = this._mediaQuery.isMobile.subscribe((isMobile) => {
-            this.isMobile = isMobile;
-        });
-    }
-
-    ngOnDestroy(): void {
-        this.mediaQuerySubscription.unsubscribe();
+    constructor(mediaQuery: MediaQueryService) {
+        this.isMobile$ = mediaQuery.isMobile;
     }
 
     get contentTextPanel(): ContentTextPanel | undefined {
@@ -41,18 +27,6 @@ export class ContentPanelComponent implements OnInit, OnDestroy {
             card: true,
             "card-padding": true,
             "cp-root": true,
-            [this.orientation]: true,
-            "content-first": !this.textFirst,
-            "text-first": this.textFirst,
-            [`size-${this.size}`]: true,
-            [this.position]: true,
-        };
-    }
-
-    get graphicNgClass(): { [clazz: string]: boolean | undefined } {
-        return {
-            "flex-2": this.size === "l",
-            "flex-1": this.size === "m",
         };
     }
 }
