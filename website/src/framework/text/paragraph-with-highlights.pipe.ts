f09cc312ba9335d195cbcf559d3625d8d8a16b2d
Bartosz Wasiluk <b.wasiluk@codeandpepper.com>
2023-08-04 13:59:39 +0200

    Formats files added before merge

--- website/src/framework/text/paragraph-with-highlights.pipe.ts
+++ website/src/framework/text/paragraph-with-highlights.pipe.ts
@@ -4,6 +4,8 @@ import { ParagraphWithHighlights, RichText } from "typedb-web-schema";
 @Pipe({ name: "paragraphWithHighlights" })
 export class ParagraphWithHighlightsPipe implements PipeTransform {
     transform(value: string): ParagraphWithHighlights {
-        return value ? new ParagraphWithHighlights({ spans: [{ text: value, highlight: false }] }) : new ParagraphWithHighlights({ spans: [] });
+        return value
+            ? new ParagraphWithHighlights({ spans: [{ text: value, highlight: false }] })
+            : new ParagraphWithHighlights({ spans: [] });
     }
 }
