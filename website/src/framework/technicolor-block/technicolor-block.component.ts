406836cc78ae62e5ef7db06ed05edcf8079d49bc
Bartosz Wasiluk <b.wasiluk@codeandpepper.com>
2023-08-07 18:53:29 +0200

    Improves technicolor dot animation

--- website/src/framework/technicolor-block/technicolor-block.component.ts
+++ website/src/framework/technicolor-block/technicolor-block.component.ts
@@ -26,12 +26,58 @@ export class TechnicolorBlockComponent implements AfterViewInit {
         const handleScroll = () =>
             dotEls.forEach((dotEl) => {
                 if (dotEl.parentElement) {
+                    const opacityChangeDistance = 30;
                     const parentRect = dotEl.parentElement.getBoundingClientRect();
                     const offsetTop = window.innerHeight / 2 - parentRect.top;
                     const opacity = Math.max(
-                        Math.min((parentRect.height / 2 + 20 - Math.abs(parentRect.height / 2 - offsetTop)) / 20, 1),
+                        Math.min(
+                            (parentRect.height / 2 +
+                                opacityChangeDistance -
+                                Math.abs(parentRect.height / 2 - offsetTop)) /
+                                opacityChangeDistance,
+                            1,
+                        ),
                         0,
                     );
+                    const keyframes: (Keyframe & { offset: number })[] = [
+                        { top: `${dotEl.offsetTop}px`, offset: 0 },
+                        { top: `${offsetTop}px`, opacity, offset: 1 },
+                    ];
+                    const topHiddenOffset = (-opacityChangeDistance - dotEl.offsetTop) / (offsetTop - dotEl.offsetTop);
+                    const topVisibleOffset = (0 - dotEl.offsetTop) / (offsetTop - dotEl.offsetTop);
+                    const bottomVisibleOffset = (parentRect.height - dotEl.offsetTop) / (offsetTop - dotEl.offsetTop);
+                    const bottomHiddenOffset =
+                        (parentRect.height + opacityChangeDistance - dotEl.offsetTop) / (offsetTop - dotEl.offsetTop);
+                    if (topHiddenOffset < 1 && topHiddenOffset > 0) {
+                        keyframes.push({
+                            opacity: 0,
+                            top: `${-opacityChangeDistance}px`,
+                            offset: topHiddenOffset,
+                        });
+                    }
+                    if (topVisibleOffset < 1 && topVisibleOffset > 0) {
+                        keyframes.push({
+                            opacity: 1,
+                            top: `0px`,
+                            offset: topVisibleOffset,
+                        });
+                    }
+                    if (bottomVisibleOffset < 1 && bottomVisibleOffset > 0) {
+                        keyframes.push({
+                            opacity: 1,
+                            top: `${parentRect.height}px`,
+                            offset: bottomVisibleOffset,
+                        });
+                    }
+                    if (bottomHiddenOffset < 1 && bottomHiddenOffset > 0) {
+                        keyframes.push({
+                            opacity: 0,
+                            top: `${parentRect.height + opacityChangeDistance}px`,
+                            offset: bottomHiddenOffset,
+                        });
+                    }
+                    keyframes.sort((a, b) => a.offset - b.offset);
+                    dotEl.animate(keyframes, { duration: 300, easing: "ease" });
                     dotEl.style.top = `${offsetTop}px`;
                     dotEl.style.opacity = `${opacity}`;
                 }
@@ -40,8 +86,8 @@ export class TechnicolorBlockComponent implements AfterViewInit {
             window.removeEventListener("scroll", handleScroll);
             window.removeEventListener("resize", handleScroll);
         });
-        window.addEventListener("scroll", handleScroll, { passive: true });
-        window.addEventListener("resize", handleScroll, { passive: true });
+        window.addEventListener("scroll", handleScroll);
+        window.addEventListener("resize", handleScroll);
         handleScroll();
     }
 
