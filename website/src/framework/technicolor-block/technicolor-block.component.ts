a9b7eb20a6e2b5041ef563f5f0e82e40d8a65e34
Bartosz Wasiluk <b.wasiluk@codeandpepper.com>
2023-08-16 14:49:59 +0200

    Technicolor Chain Dots (#245)
    ## What is the goal of this PR?
    
    Added dot following user's scroll position to Technicolor Block.
    
    ## What are the changes implemented in this PR?
    
    - added default z-index to section,
    - simplified color classes,
    - added dot element,
    - added event listener for scroll/resize to update dot's opacity and
    position.

--- website/src/framework/technicolor-block/technicolor-block.component.ts
+++ website/src/framework/technicolor-block/technicolor-block.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { AfterViewInit, Component, DestroyRef, ElementRef, Input } from "@angular/core";
 import { TechnicolorBlock } from "typedb-web-schema";
 
 @Component({
@@ -6,7 +6,7 @@ import { TechnicolorBlock } from "typedb-web-schema";
     templateUrl: "technicolor-block.component.html",
     styleUrls: ["./technicolor-block.component.scss"],
 })
-export class TechnicolorBlockComponent {
+export class TechnicolorBlockComponent implements AfterViewInit {
     @Input() block!: TechnicolorBlock;
     @Input() index!: number;
     @Input() size: "medium" | "large" = "medium";
@@ -16,27 +16,28 @@ export class TechnicolorBlockComponent {
     @Input() noBody?: boolean;
     @Input() longUpperChain?: boolean;
 
-    get graphicLineColorClass(): string {
-        switch (this.index % 3) {
-            case 0:
-                return "tb-graphic-line-green";
-            case 1:
-                return "tb-graphic-line-pink";
-            case 2:
-                return "tb-graphic-line-yellow";
-            default:
-                throw "Unreachable code";
-        }
+    private readonly opacityChangeDistance = 15;
+
+    constructor(
+        private destroyRef: DestroyRef,
+        private elementRef: ElementRef<HTMLElement>,
+    ) {}
+
+    ngAfterViewInit(): void {
+        const { cleanupDotListeners } = this.initDotListeners();
+        this.destroyRef.onDestroy(() => {
+            cleanupDotListeners();
+        });
     }
 
-    get graphicIconBgColorClass(): string {
+    get graphicColorClass(): string {
         switch (this.index % 3) {
             case 0:
-                return "tb-graphic-icon-bg-green";
+                return "tb-graphic-green";
             case 1:
-                return "tb-graphic-icon-bg-pink";
+                return "tb-graphic-pink";
             case 2:
-                return "tb-graphic-icon-bg-yellow";
+                return "tb-graphic-yellow";
             default:
                 throw "Unreachable code";
         }
@@ -71,5 +72,65 @@ export class TechnicolorBlockComponent {
             case "medium":
                 return "tb-text-m text-24-32 text-20-32-tablet text-14-21-mobile";
         }
+    }
+
+    private initDotListeners() {
+        const dotEls = this.elementRef.nativeElement.querySelectorAll<HTMLElement>(".tb-graphic-dot");
+        const handleScroll = () => dotEls.forEach((dotEl) => this.updateDotPosition(dotEl));
+        window.addEventListener("scroll", handleScroll);
+        window.addEventListener("resize", handleScroll);
+        handleScroll();
+        const cleanupDotListeners = () => {
+            window.removeEventListener("scroll", handleScroll);
+            window.removeEventListener("resize", handleScroll);
+        };
+        return { cleanupDotListeners };
+    }
+
+    private updateDotPosition(dotEl: HTMLElement): void {
+        if (dotEl.parentElement) {
+            const { height: parentHeight, top: parentOffset } = dotEl.parentElement.getBoundingClientRect();
+            const initialOffset = dotEl.offsetTop;
+            const keyframes = this.calculateDotAnimationKeyframes({ initialOffset, parentHeight, parentOffset });
+            const { top: finalOffset, opacity: finalOpacity } = keyframes[keyframes.length - 1];
+
+            dotEl.animate(keyframes, { duration: 300, easing: "ease" });
+            dotEl.style.top = `${finalOffset}`;
+            dotEl.style.opacity = `${finalOpacity}`;
+        }
+    }
+
+    private calculateDotAnimationKeyframes({
+        initialOffset,
+        parentHeight,
+        parentOffset,
+    }: {
+        initialOffset: number;
+        parentHeight: number;
+        parentOffset: number;
+    }): Keyframe[] {
+        const finalOffset = window.innerHeight / 2 - parentOffset;
+        const opacity = 1 + (parentHeight / 2 - Math.abs(parentHeight / 2 - finalOffset)) / this.opacityChangeDistance;
+        const finalOpacity = Math.max(Math.min(opacity, 1), 0);
+
+        const topHiddenOffset = (-this.opacityChangeDistance - initialOffset) / (finalOffset - initialOffset);
+        const topVisibleOffset = (0 - initialOffset) / (finalOffset - initialOffset);
+        const bottomVisibleOffset = (parentHeight - initialOffset) / (finalOffset - initialOffset);
+        const bottomHiddenOffset =
+            (parentHeight + this.opacityChangeDistance - initialOffset) / (finalOffset - initialOffset);
+
+        const calculatedKeyframes: (Keyframe & { offset: number })[] = [
+            { opacity: 0, top: `${-this.opacityChangeDistance}px`, offset: topHiddenOffset },
+            { opacity: 1, top: `0px`, offset: topVisibleOffset },
+            { opacity: 1, top: `${parentHeight}px`, offset: bottomVisibleOffset },
+            { opacity: 0, top: `${parentHeight + this.opacityChangeDistance}px`, offset: bottomHiddenOffset },
+        ].filter(({ offset }) => offset > 0 && offset < 1);
+
+        const allKeyframes = calculatedKeyframes.concat(
+            { top: `${initialOffset}px`, offset: 0 },
+            { top: `${finalOffset}px`, opacity: finalOpacity, offset: 1 },
+        );
+
+        return allKeyframes.sort((a, b) => a.offset - b.offset);
     }
 }
