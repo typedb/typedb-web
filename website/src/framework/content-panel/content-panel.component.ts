cdf0a55c53982e8c9f5196b301230cfe9bd99ac9
Alex Walker <alex@vaticle.com>
2023-08-31 15:37:42 +0100

    Standardise card padding across home page

--- website/src/framework/content-panel/content-panel.component.ts
+++ website/src/framework/content-panel/content-panel.component.ts
@@ -24,8 +24,7 @@ export class ContentPanelComponent {
     get rootNgClass(): { [clazz: string]: boolean | undefined } {
         return {
             section: true,
-            card: true,
-            "card-padding": true,
+            "card": true,
             "cp-root": true,
         };
     }
