4240d4971d68cb80e6cf8d6587174385cb8055c1
Bartosz Wasiluk <b.wasiluk@codeandpepper.com>
2023-09-12 17:36:53 +0200

    Show event start date/time and its duration (#264)
    ## What is the goal of this PR?
    
    Previously just start date or `TBC` was displayed.
    
    Now it shows also time component and event duration.
    To make space to support both date and duration, event tag is not
    displayed.
    
    ## What are the changes implemented in this PR?
    
    - removed dateRange pipe in favour of eventDate,
    - extended event duration to support event date options object.

--- website/src/framework/date/event-duration.pipe.ts
+++ website/src/framework/date/event-duration.pipe.ts
@@ -1,15 +1,45 @@
 import { Pipe, PipeTransform } from "@angular/core";
+import { EventDate } from "typedb-web-schema";
 
 @Pipe({
     name: "eventDuration",
 })
 export class EventDurationPipe implements PipeTransform {
-    transform(durationMins: number): string {
-        if (!durationMins) {
+    transform(val: number | EventDate): string {
+        if (typeof val === "object") {
+            if (!val.startDate || !val.endDate) {
+                return "";
+            }
+            const durationMins = (+val.endDate - +val.startDate) / 1000 / 60;
+
+            if (durationMins < 60 * 12) {
+                return this.formatDurationMins(durationMins);
+            }
+
+            const startDate = new Date(val.startDate);
+            const endDate = new Date(val.endDate);
+            startDate.setHours(0, 0, 0, 0);
+            endDate.setHours(0, 0, 0, 0);
+            const durationDays = Math.round((+endDate - +startDate) / 1000 / 60 / 60 / 24) + 1;
+
+            return `${durationDays} day${durationDays === 1 ? "" : "s"}`;
+        }
+        return this.formatDurationMins(val);
+    }
+
+    private formatDurationMins(val: number) {
+        if (!val) {
             return "";
         }
-        if (durationMins < 60) return `${durationMins} minutes`;
-        else if (durationMins > 60) return `${durationMins / 60} hours`;
-        else return "1 hour";
+        if (val < 60) {
+            return `${val} minutes`;
+        }
+        if (val === 60) {
+            return `1 hour`;
+        }
+        if (val < 60 * 12) {
+            return `${val / 60} hours`;
+        }
+        return "";
     }
 }
