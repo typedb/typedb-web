545cbbe1685b7c124f8e508c46cedf18aa431dad
Alex Walker <alex@vaticle.com>
2023-10-13 16:36:22 +0100

    lint fix

--- website/src/framework/publication-panel/publication-panel.component.ts
+++ website/src/framework/publication-panel/publication-panel.component.ts
@@ -12,10 +12,12 @@ import {
     selector: "td-publication-panel",
     templateUrl: "./publication-panel.component.html",
     styleUrls: ["./publication-panel.component.scss"],
-    host: { class: "section card-appearance" },
 })
 export class PublicationPanelComponent {
     @Input() items!: PublicationPanelItem[];
+
+    @HostBinding("class.section") useSectionClass = true;
+    @HostBinding("class.card-appearance") useCardAppearanceClass = true;
 
     isContentRow(item: PublicationPanelItem): item is PublicationContentRow {
         return item instanceof PublicationContentRow;
