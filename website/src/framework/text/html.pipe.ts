66531e7466990af1cce3b2dee2cc8c532426e654
Alex Walker <alex@vaticle.com>
2023-10-17 13:47:34 +0100

    Auto-detect if Portable Text links should open in new tab

--- website/src/framework/text/html.pipe.ts
+++ website/src/framework/text/html.pipe.ts
@@ -6,6 +6,16 @@ import { PortableText } from "typedb-web-schema";
 @Pipe({ name: "html" })
 export class HtmlPipe implements PipeTransform {
     transform(value: PortableText): string {
-        return toHTML(value);
+        const initialHtmlString = toHTML(value);
+        const element = new DOMParser().parseFromString(initialHtmlString, "text/html");
+        const anchorEls = element.getElementsByTagName("a");
+        for (const i in anchorEls) {
+            if (!anchorEls.hasOwnProperty(i)) continue;
+            const anchorEl = anchorEls[i];
+            if (!anchorEl.href.startsWith(window.location.origin)) {
+                anchorEl.target = "_blank";
+            }
+        }
+        return element.body.innerHTML.toString();
     }
 }
