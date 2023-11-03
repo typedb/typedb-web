a4116341ae83bc3b24a9d4713d47c336ffd65c20
Bartosz Wasiluk <b.wasiluk@codeandpepper.com>
2023-11-03 12:12:58 +0100

    Adds json highlighter

--- common/src/scripts/prism.ts
+++ common/src/scripts/prism.ts
@@ -2,6 +2,7 @@ import "prismjs";
 import PrismType from "prismjs";
 import "prismjs/components/prism-cypher";
 import "prismjs/components/prism-java";
+import "prismjs/components/prism-json";
 import "prismjs/components/prism-python";
 import "prismjs/components/prism-rust";
 import "prismjs/components/prism-sql";
