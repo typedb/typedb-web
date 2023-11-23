1f0ce9beae30cad4f5bad3852dbed0818efd94d8
Bartosz Wasiluk <b.wasiluk@codeandpepper.com>
2023-11-23 14:35:00 +0100

    Improves common init error handling

--- common/bin/init.js
+++ common/bin/init.js
@@ -4,5 +4,6 @@ const path = require("path");
 try {
     execSync("pnpm build", { cwd: path.dirname(__dirname) });
 } catch (err) {
-    console.error(new Error(err.stderr?.toString() || err));
+    console.error(err.stdout?.toString() || err);
+    process.exit(1);
 }
