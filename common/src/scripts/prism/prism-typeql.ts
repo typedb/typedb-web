59ae5002a5edad40ae2b75871c18d08d579d4728
cxdorn <christophxdorn@gmail.com>
2024-03-06 10:50:54 +0000

    Negative lookbehind!

--- common/src/scripts/prism/prism-typeql.ts
+++ common/src/scripts/prism/prism-typeql.ts
@@ -39,7 +39,7 @@ Prism.languages["typeql"] = {
         alias: "datetime",
     },
     number: {
-        pattern: /((?!-)\b| -)[0-9]+(\.[0-9][0-9]*)?\b/,
+        pattern: /((?<!-)\b| -)[0-9]+(\.[0-9][0-9]*)?\b/,
         alias: "number",
     },
     // number: {
