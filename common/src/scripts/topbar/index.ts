58a8d517e33ed492a40e679cf272b16542f8d030
Alex Walker <alex@vaticle.com>
2024-05-20 15:08:22 +0100

    Export platform UI banner query properly

--- common/src/scripts/topbar/index.ts
+++ common/src/scripts/topbar/index.ts
@@ -1,4 +1,5 @@
 export * from "./generate-topbar";
 export * from "./get-topbar-data";
+export * from "./platform-ui-banner-query";
 export * from "./setup-topbar-listeners";
 export { TopbarData, topbarQuery } from "./topbar-query";
