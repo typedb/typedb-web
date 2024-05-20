82c42d4f0b39a6c733777a0e46cbb378be253b98
Alex Walker <alex@vaticle.com>
2024-05-20 15:51:41 +0100

    Expose Sanity API CDN URL

--- common/src/scripts/sanity-config.ts
+++ common/src/scripts/sanity-config.ts
@@ -1,5 +1,8 @@
-const SANITY_URL = "https://xndl14mc.api.sanity.io/";
+const SANITY_PROJECT_ID = `xndl14mc`;
+const SANITY_URL = `https://${SANITY_PROJECT_ID}.api.sanity.io/`;
 export const SANITY_QUERY_URL = `${SANITY_URL}/v2021-10-21/data/query/production`;
+const SANITY_CDN_URL = `https://${SANITY_PROJECT_ID}.apicdn.sanity.io/`;
+export const SANITY_CDN_QUERY_URL = `${SANITY_CDN_URL}/v2021-10-21/data/query/production`;
 // Read-only API token that must be provided to read draft content
 export const SANITY_TOKEN =
     "skIRNgnaiMLWn9XUwl20yvPaUODDE4P6kNWiRicQEthG2J4wvcCA1vRaCkTC9y4SChNzoq8BAw2vRuDEKvXRayMbVgUFsuER7otBti0zDzDk6mrEPze4oDfEPYyiw9eklL352jwFXVELvHNESrvkRiAm5IDxECjN3aYM3JjNH7bWbp5czrw3";
