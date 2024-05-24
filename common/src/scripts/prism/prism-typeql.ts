1cb009829960f034f5953f8de76f680f6c2ecd17
Christoph Dorn <christoph@typedb.com>
2024-05-24 14:39:42 +0100

    Keyword `first` (#326)
    ## Release notes: usage and product changes
    
    Adding missing keyword `first`.
    
    ## Implementation
    
    Added in prism-typeql config.

--- common/src/scripts/prism/prism-typeql.ts
+++ common/src/scripts/prism/prism-typeql.ts
@@ -11,7 +11,7 @@ Prism.languages["typeql"] = {
         pattern: /(".*?")|('.*?')/,
     },
     keyword: {
-        pattern: /((?:(?![-a-zA-Z_0-9]|\$|\?).)|^|\s)(define|undefine|match|with|fun|struct|return|reduce|get|filter|assert|insert|delete|put|std|median|mean|max|min|sum|count|group|where|limit|offset|sort|asc|desc|when|then|fetch|rule|like|floor|ceil|round|abs)(?![-a-zA-Z_0-9])/,
+        pattern: /((?:(?![-a-zA-Z_0-9]|\$|\?).)|^|\s)(define|undefine|match|with|fun|struct|return|reduce|get|filter|assert|insert|delete|put|std|median|mean|max|min|first|sum|count|group|where|limit|offset|sort|asc|desc|when|then|fetch|rule|like|floor|ceil|round|abs)(?![-a-zA-Z_0-9])/,
         lookbehind: true,
     },
     constraint: {
