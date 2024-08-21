b37d1d6b834f79e575070024349038dc2275c6ef
Alex Walker <alex@vaticle.com>
2024-08-21 16:15:08 +0100

    Add edge function "echo" for debugging webhooks, etc.

---
+++ website/netlify/edge-functions/echo/echo.ts
@@ -0,0 +1,8 @@
+/* This endpoint exists for debugging purposes - e.g. to view the payload of a webhook invocation. */
+
+import type { Context } from "https://edge.netlify.com";
+
+export default async (request: Request, context: Context) => {
+    console.log(JSON.stringify(request.body));
+    return new Response(null, { status: 202 });
+};
