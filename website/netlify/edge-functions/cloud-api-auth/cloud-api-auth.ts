f960ea4c421e87ade6137fe2ed0b008db19e67d0
Sam Butcher <samuel@typedb.com>
2025-01-28 14:44:53 +0000

    Add edge function for authenticating the TypeDB Cloud API (#336)
    ## Release notes: usage and product changes
    
    We add a function that accepts an API token client ID and client secret,
    and exchanges them for an ID token from google identity platform.
    
    ## Implementation
    
    The function extracts a client ID and client secret from a basic auth
    header, and uses them to construct an appropriate request to google
    identity platform. If the response is not `ok`, we return it as-is -
    otherwise we extract the `idToken` and return that.
    
    We specify separate API keys and tenant IDs for each environment in the
    `netlify.toml`, to allow it to operate correctly for development,
    staging, and production.

---
+++ website/netlify/edge-functions/cloud-api-auth/cloud-api-auth.ts
@@ -0,0 +1,32 @@
+/*
+ * This unpublished material is proprietary to Vaticle.
+ * All rights reserved. The methods and
+ * techniques described herein are considered trade secrets
+ * and/or confidential. Reproduction or distribution, in whole
+ * or in part, is forbidden except by express written permission
+ * of Vaticle.
+ */
+
+import type { Context } from "https://edge.netlify.com";
+
+export default async (request: Request, context: Context) => {
+    if (request.method !== "POST") return undefined;
+    const apiKey = Netlify.env.get("CLOUD_API_AUTH_API_KEY");
+    const tenantId = Netlify.env.get("CLOUD_API_AUTH_TENANT_ID");
+    const auth = request.headers.get("Authorization")!.replace("Basic ", "");
+    const clientID = auth.split(":")[0];
+    const clientSecret = auth.split(":")[1];
+    const response = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`, {
+        method: "POST",
+        body: JSON.stringify({
+            email: `${clientID}@serviceaccounts.cloud.typedb.com`,
+            password: clientSecret,
+            tenantId,
+            returnSecureToken: true,
+        }),
+        headers: { "Content-Type": "application/json" },
+    });
+    if (!response.ok) return response;
+    const body = await response.json();
+    return new Response(body.idToken);
+};
