51304d1c3d6ba894de18e8c45b34e29e37d92be2
sam-butcher <samuel@typedb.com>
2025-01-31 14:59:10 +0000

    Separate edge functions per cloud auth environment

--- website/netlify/edge-functions/cloud-api-auth-dev/cloud-api-auth-dev.ts
+++ website/netlify/edge-functions/cloud-api-auth-dev/cloud-api-auth-dev.ts
@@ -11,7 +11,7 @@ import type { Context } from "https://edge.netlify.com";
 import { authApi } from "../../cloud-api-auth-common/cloud-api-auth-common.ts";
 
 export default async (request: Request, context: Context) => {
-    const tenantId = "platform-api-utdw2";
-    const apiKey = "AIzaSyBwx6DlQYNejTVUtjzQetj15frQ_FblVT8";
+    const tenantId = "platform-api-kjfbu";
+    const apiKey = "AIzaSyDJHINOCzIvZpr-_CC8dtvWj4un_uuVysE";
     return authApi(request, tenantId, apiKey);
 };
--- website/netlify/edge-functions/cloud-api-auth-staging/cloud-api-auth-staging.ts
+++ website/netlify/edge-functions/cloud-api-auth-staging/cloud-api-auth-staging.ts
@@ -11,7 +11,7 @@ import type { Context } from "https://edge.netlify.com";
 import { authApi } from "../../cloud-api-auth-common/cloud-api-auth-common.ts";
 
 export default async (request: Request, context: Context) => {
-    const tenantId = "platform-api-utdw2";
-    const apiKey = "AIzaSyBwx6DlQYNejTVUtjzQetj15frQ_FblVT8";
+    const tenantId = "platform-api-tnz9v";
+    const apiKey = "AIzaSyBUcSa59YGemKHQb1UVT9nBk7H-yZCdJQ4";
     return authApi(request, tenantId, apiKey);
 };
