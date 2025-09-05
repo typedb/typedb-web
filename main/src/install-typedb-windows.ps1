92aa3ee653f7235a8ebd33c588e3b38a622aee04
Alex Walker <alex@typedb.com>
2025-09-05 20:52:05 +0100

    Install typedb PS1: Add note about allowing network access when prompted to run CE locally

--- main/src/install-typedb-windows.ps1
+++ main/src/install-typedb-windows.ps1
@@ -24,7 +24,7 @@ Write-Host "Get started using the following commands:" -ForegroundColor Cyan
 Write-Host ""
 Write-Host "  View TypeDB Console CLI options:" -ForegroundColor White
 Write-Host "    typedb console --help" -ForegroundColor Gray
-Write-Host "  Run a local database server:" -ForegroundColor White
+Write-Host "  Run a local database server (you'll need to allow network access when prompted):" -ForegroundColor White
 Write-Host "    typedb server" -ForegroundColor Gray
 Write-Host "  Read the docs:" -ForegroundColor White
 Write-Host "    https://typedb.com/docs/home/get-started/" -ForegroundColor Gray
