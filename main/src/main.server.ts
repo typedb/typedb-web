ae4227db88911fcb5bd61bbcd1da4b1b6c948392
Alex Walker <alex@typedb.com>
2025-09-12 17:44:31 +0100

    Re-enable Intercom, Posthog and cookieconsent
    I had them commented out by mistake >:(

--- main/src/main.server.ts
+++ main/src/main.server.ts
@@ -1,31 +1,19 @@
-// import { bootstrapApplication } from '@angular/platform-browser';
-// import { serverConfig } from "./config.server";
-// import { RootComponent } from "./root.component";
-//
-// const bootstrap = () => {
-//     // throw new Error("main.server.ts: bootstrapApplication");
-//     return bootstrapApplication(RootComponent, serverConfig);
-// }
-//
-// export default bootstrap;
-
 import "zone.js";
 import 'zone.js/node';
 import { DOMParser } from '@xmldom/xmldom';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { getServerConfig } from "./config.server";
+import { RootComponent } from "./root.component";
 
 if (typeof global !== 'undefined') {
     (global as any).DOMParser = DOMParser;
 }
 
-import { bootstrapApplication } from '@angular/platform-browser';
-import { serverConfigPromise } from "./config.server";
-import { RootComponent } from "./root.component";
-
 // This is the entry point for the server-side rendering
 export default async function bootstrap() {
   try {
     // Wait for both the server config and app config to be ready
-    const serverConfig = await serverConfigPromise;
+    const serverConfig = await getServerConfig();
 
     // Merge the server and app configs
     const mergedConfig = {
