17033cd8dfb3c5dde2220d4282813b2b350d4e73
Alex Walker <alex@typedb.com>
2025-12-16 15:18:32 +0000

    Fix hydration breaking Prism

--- main/src/framework/code/syntax-highlight.directive.ts
+++ main/src/framework/code/syntax-highlight.directive.ts
@@ -1,5 +1,5 @@
-import { DOCUMENT } from "@angular/common";
-import { Directive, ElementRef, Input, AfterViewInit, OnChanges, inject } from '@angular/core';
+import { DOCUMENT, isPlatformBrowser } from "@angular/common";
+import { Directive, ElementRef, Input, AfterViewInit, OnChanges, SimpleChanges, inject, PLATFORM_ID } from '@angular/core';
 import * as Prism from 'prismjs';
 
 @Directive({
@@ -11,16 +11,24 @@ export class SyntaxHighlightDirective implements AfterViewInit, OnChanges {
   @Input({ required: true }) language = '';
   private el = inject(ElementRef);
   private document = inject(DOCUMENT);
+  private platformId = inject(PLATFORM_ID);
+  private hasRendered = false;
 
   ngAfterViewInit() {
-    this.highlight();
+    if (!this.hasRendered) {
+      this.highlight();
+    }
   }
 
-  ngOnChanges() {
-    this.highlight();
+  ngOnChanges(changes: SimpleChanges) {
+    if ((changes['code'] || changes['language']) && this.hasRendered) {
+      this.highlight();
+    }
   }
 
   private highlight() {
+    // Skip if not in browser
+    if (!isPlatformBrowser(this.platformId)) return;
     if (!this.el.nativeElement) return;
 
     const pre = this.document.createElement('pre');
@@ -33,5 +41,6 @@ export class SyntaxHighlightDirective implements AfterViewInit, OnChanges {
     this.el.nativeElement.appendChild(pre);
 
     Prism.highlightElement(codeEl);
+    this.hasRendered = true;
   }
 }
