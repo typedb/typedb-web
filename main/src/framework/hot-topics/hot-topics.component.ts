cd7c710f1e70c086cb794ef186f6d636e2f8e875
Alex Walker <alex@typedb.com>
2025-12-22 14:59:11 +0000

    Compress footer a little. Hot topics section now supports actions

--- main/src/framework/hot-topics/hot-topics.component.ts
+++ main/src/framework/hot-topics/hot-topics.component.ts
@@ -1,10 +1,11 @@
 import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, HostBinding, Input, ViewChild } from "@angular/core";
-import { ParagraphWithHighlights, ResourceLink } from "typedb-web-schema";
+import { ActionButton, LinkButton, ParagraphWithHighlights, ResourceLink } from "typedb-web-schema";
 import { MediaQueryService } from "../../service/media-query.service";
 import { LinkDirective } from "../link/link.directive";
 import { ScrollPaneComponent } from "../scroll-pane/scroll-pane.component";
 import { RichTextComponent } from "../text/rich-text.component";
 import { HeadingWithHighlightsComponent } from "../text/text-with-highlights.component";
+import { ActionsComponent } from "../actions/actions.component";
 
 @Component({
     selector: 'td-hot-topics',
@@ -13,11 +14,13 @@ import { HeadingWithHighlightsComponent } from "../text/text-with-highlights.com
     standalone: true,
     imports: [
         ScrollPaneComponent, HeadingWithHighlightsComponent, LinkDirective, RichTextComponent,
-    ],
+        ActionsComponent
+],
 })
 export class HotTopicsComponent {
     @Input() title!: ParagraphWithHighlights;
     @Input() resources!: ResourceLink[];
+    @Input() actions?: ActionButton[];
     @Input() appearance: "news" | "resources" = "news";
     cardWidth = 384;
 
